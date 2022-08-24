2e4b16d6dd8329dc456a4fe82ec3b179c2de44fd
evilusean <82889521+evilusean@users.noreply.github.com>
2022-08-24 10:16:40 -0300

    Create Lab7FindJane.sh

---
+++ IT-Automation-With-Python/2Using Python to Interact with the Operating System/Lab7FindJane.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+>oldFiles.txt
+
+files=$(grep " jane " /home/student-00-b5c7881ca4c3/data/list.txt | cut -d ' ' -f 3)
+
+for i in $files;do
+
+        if test -e ~/$i;then
+
+                echo $i>>oldFiles.txt;
+
+        else
+                echo "not working"; fi
+
+done
+#####Not Working
+#New Script
+#!/bin/bash
+>oldFiles.txt
+
+files=$(grep " jane " /home/student-00-b5c7881ca4c3/data/list.txt | cut -d ' ' -f 3)
+
+for file in $files; do
+if test -e ~/$file; then echo $file >> oldFiles.txt; else echo "File does not exist";$
+done
+###Not Working
+#New Script(WORKS) ./findJane > oldFiles.txt
+#!/bin/bash
+>oldFiles.txt
+
+files=$(grep " jane " /home/student-00-b5c7881ca4c3/data/list.txt | cut -d ' ' -f 3)
+
+for file in $files; do
+
+        if test -e ~/data/$file; then
+                echo $file >> oldFiles.txt;
+        else
+                echo "File doesn't exist"; fi
+done
+

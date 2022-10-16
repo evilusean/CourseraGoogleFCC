4839649bf20add920cea5beba0442786eadcd7e2
evilusean <82889521+evilusean@users.noreply.github.com>
2022-10-16 09:09:56 -0300

    Update Lab3cars.py

--- IT-Automation-With-Python/6Automating Real-World Tasks with Python/Lab3cars.py
+++ IT-Automation-With-Python/6Automating Real-World Tasks with Python/Lab3cars.py
@@ -46,6 +46,30 @@ def process_data(data):
   return summary
 
 
+def cars_dict_to_table(car_data):
+  """Turns the data in car_data into a list of lists."""
+  table_data = [["ID", "Car", "Price", "Total Sales"]]
+  for item in car_data:
+    table_data.append([item["id"], format_car(item["car"]), item["price"], item["total_sales"]])
+  return table_data
+
+
+def main(argv):
+  """Process the JSON data and generate a full report out of it."""
+  data = load_data("car_sales.json")
+  summary = process_data(data)
+  print(summary)
+  # TODO: turn this into a PDF report
+
+  # TODO: send the PDF report as an email attachment
+
+
+if __name__ == "__main__":
+  main(sys.argv)
+
+
+
+
 
 """Attempt 1"""
 #!/usr/bin/env python3

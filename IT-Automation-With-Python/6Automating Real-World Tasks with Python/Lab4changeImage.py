e497839eed87fd100d0607726665c77bc6bd6ece
evilusean <82889521+evilusean@users.noreply.github.com>
2022-10-21 08:00:17 -0300

    Update Lab4changeImage.py

--- IT-Automation-With-Python/6Automating Real-World Tasks with Python/Lab4changeImage.py
+++ IT-Automation-With-Python/6Automating Real-World Tasks with Python/Lab4changeImage.py
@@ -7,3 +7,13 @@ Format: Change image format from .TIFF to .JPEG
 After processing the images, save them in the same path ~/supplier-data/images, with a JPEG extension.
 """
 #!/usr/bin/env python3
+
+from PIL import Image
+import os
+import sys
+
+#gets long alphanumeric username
+user = os.getenv('USER')
+
+#image directory
+image_directory = '/home/{}/supplier-data/images/'.format(user)

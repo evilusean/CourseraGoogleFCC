fc5d791335d308fde19a81a65fa3ba05a04ca0df
evilusean <82889521+evilusean@users.noreply.github.com>
2022-10-22 08:19:28 -0300

    Update Lab4suppler_image_upload.py

--- IT-Automation-With-Python/6Automating Real-World Tasks with Python/Lab4suppler_image_upload.py
+++ IT-Automation-With-Python/6Automating Real-World Tasks with Python/Lab4suppler_image_upload.py
@@ -17,5 +17,18 @@ image_directory = '/home/{}/supplier-data/images/'.format(user)
 
 url = "http://localhost/upload/"
 
-with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
+#Listing all files in image directory
+files = os.listdir(image_directory)
+
+#parse through all images
+for image_name in files:
+
+    #gets all images in jpeg format
+    if not image_name.startswith('.') and 'jpeg' in image_name:
+
+        #create path for each image
+        image_path = image_directory + image_name
+
+        #uploads jpeg to webserver
+        with open(image_path, 'rb') as opened:
+            r = requests.post(url, files={'file': opened})

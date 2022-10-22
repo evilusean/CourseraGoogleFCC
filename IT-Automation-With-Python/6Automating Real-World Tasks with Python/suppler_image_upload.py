8ab6d014011efd0a91640dddea0177317d49e01a
evilusean <82889521+evilusean@users.noreply.github.com>
2022-10-22 07:45:28 -0300

    Create suppler_image_upload.py

---
+++ IT-Automation-With-Python/6Automating Real-World Tasks with Python/suppler_image_upload.py
@@ -0,0 +1,16 @@
+"""
+In a similar way, you are going to write a script named supplier_image_upload.py that takes the jpeg images from the supplier-data/images directory
+that you've processed previously and uploads them to the web server fruit catalog.
+
+Complete the script with the same technique as used in the file example_upload.py.
+"""
+#!/usr/bin/env python3
+
+import os
+import requests
+
+#gets long alphanumeric username
+user = os.getenv('USER')
+
+#image directory
+image_directory = '/home/{}/supplier-data/images/'.format(user)

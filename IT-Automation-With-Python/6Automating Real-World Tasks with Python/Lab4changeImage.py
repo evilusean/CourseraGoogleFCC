518ef7284bf01903a3454191612ac971bbe59fcc
evilusean <82889521+evilusean@users.noreply.github.com>
2022-10-22 07:24:37 -0300

    Update Lab4changeImage.py

--- IT-Automation-With-Python/6Automating Real-World Tasks with Python/Lab4changeImage.py
+++ IT-Automation-With-Python/6Automating Real-World Tasks with Python/Lab4changeImage.py
@@ -17,3 +17,22 @@ user = os.getenv('USER')
 
 #image directory
 image_directory = '/home/{}/supplier-data/images/'.format(user)
+
+for image_name in os.listdir(image_directory):
+    #ignores images that don't have .tiff format
+    if not image_name.startswith('.') and 'tiff' in image_name:
+
+        #creates path variable
+        image_path = image_directory + image_name
+
+        #split and take first indexed part
+        path = os.path.splitext(image_path)[0]
+
+        #Open the image
+        im = Image.open(image_path)
+
+        #create new path with jpeg format
+        new_path = '{}.jpeg'.format(path)
+
+        #resize and then save it as jpeg
+        im.convert('RGB').resize((600, 400)).save(new_path, "JPEG")

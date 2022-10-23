6cbab929939bbf7ccddc35bce86953eefe5d9dd7
evilusean <82889521+evilusean@users.noreply.github.com>
2022-10-23 10:56:13 -0300

    Create Lab4reports.py

---
+++ IT-Automation-With-Python/6Automating Real-World Tasks with Python/Lab4reports.py
@@ -0,0 +1,9 @@
+"""
+Create a script reports.py to generate PDF report to supplier using the nano editor:
+Using the reportlab Python library, define the method generate_report to build the PDF reports.
+We have already covered how to generate PDF reports in an earlier lesson; you will want to use similar concepts to create a PDF report named 
+processed.pdf.
+
+"""
+#!/usr/bin/env python3
+import ReportLab

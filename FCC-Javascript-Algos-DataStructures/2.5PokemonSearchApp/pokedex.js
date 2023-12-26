78e69a90046becc3d5bc0862917de6227df1f3ba
evilusean <82889521+evilusean@users.noreply.github.com>
2023-12-26 01:32:06 -0400

    added more js
    need to figure out how to pull and post data after making new URL from input

--- FCC-Javascript-Algos-DataStructures/2.5PokemonSearchApp/pokedex.js
+++ FCC-Javascript-Algos-DataStructures/2.5PokemonSearchApp/pokedex.js
@@ -1,2 +1,9 @@
 const API_URL = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon"
-const pokemon = document.getElementById("search-pokemon")
+const pokemon = document.getElementById("search-input")
+const pokemon_URL = API_URL + pokemon
+addEventListener = document.getElementById("search-button")
+
+ // Need to add API_URL + pokemon to get data
+ // Need to update all divs in index with pokemon data - how to post?
+ // Need to add event listener to search button
+// Need to add "pokemon not found" for NA

2dd9e68e41ce9e2928e717b3656032eb66ebd900
evilusean <82889521+evilusean@users.noreply.github.com>
2023-12-26 01:59:28 -0400

    Update pokedex.js
    combined API_URL +pokemon to pull data, trying fetch, still need to figure out how to post

--- FCC-Javascript-Algos-DataStructures/2.5PokemonSearchApp/pokedex.js
+++ FCC-Javascript-Algos-DataStructures/2.5PokemonSearchApp/pokedex.js
@@ -1,7 +1,9 @@
 const API_URL = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon"
-const pokemon = document.getElementById("search-input")
+const pokemon = document.getElementById("search-input").value;
 const pokemon_URL = API_URL + pokemon
 addEventListener = document.getElementById("search-button")
+
+fetch(`${API_URL}/${pokemon}`) //maybe fetch works?
 
  // Need to add API_URL + pokemon to get data
  // Need to update all divs in index with pokemon data - how to post?

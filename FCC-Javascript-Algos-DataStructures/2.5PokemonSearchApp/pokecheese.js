2ebcf617ce2f2c5838171e20852e9919be920bb1
evilusean <82889521+evilusean@users.noreply.github.com>
2023-12-28 00:14:17 -0400

    Update pokecheese.js
    tests failed, again..

--- FCC-Javascript-Algos-DataStructures/2.5PokemonSearchApp/pokecheese.js
+++ FCC-Javascript-Algos-DataStructures/2.5PokemonSearchApp/pokecheese.js
@@ -1,3 +1,4 @@
+//POKECHEEESE ALSO FAILED TESTS.. worth a shot
 const API_URL = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/";
 
 const searchButton = document.getElementById("search-button");

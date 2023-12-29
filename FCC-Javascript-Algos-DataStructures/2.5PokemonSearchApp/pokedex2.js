476ee9d3e7fa2a82edf871a49b256bfe1f37cd4e
evilusean <82889521+evilusean@users.noreply.github.com>
2023-12-29 00:38:59 -0400

    Update pokedex2.js
    added new update function, should show the searched for pokemon if found

--- FCC-Javascript-Algos-DataStructures/2.5PokemonSearchApp/pokedex2.js
+++ FCC-Javascript-Algos-DataStructures/2.5PokemonSearchApp/pokedex2.js
@@ -55,3 +55,34 @@ const lookForPokemon = async (searchStr) => {
           return result
         }
       }
+
+      const showPokemon = (pokemon) => {
+        let backColor=convertHexToRgb(colours['electric'])
+        let textColor=contrastColor(backColor)
+        if (pokemon.types[0].type.name) {
+            backColor=convertHexToRgb(colours[pokemon.types[0].type.name])
+        }
+        let backColorHTML='rgb(' + backColor.join(', ') + ')'
+        let textColorHTML='rgb(' + textColor.join(', ') + ')'
+        for (let p=0; p<=3; p++) {
+            backgrdNameId[p].style.backgroundColor = backColorHTML
+            backgrdNameId[p].style.color = textColorHTML
+        }
+        cardBorder.style.borderColor = backColorHTML
+        pokemonName.innerText = pokemon.name.toUpperCase()
+        pokemonId.innerText = pokemon.id
+        pokemonWeight.innerText = 'Weight: ' + pokemon.weight
+        pokemonHeight.innerText = 'Height: ' + pokemon.height
+        pokemonTypes[0].innerHTML = ''
+    pokemonImg.innerHTML = ''
+        for (let typeName of pokemon.types) {
+            pokemonTypes[0].innerHTML += '<h4>' + typeName.type.name.toUpperCase() + '</h4>'
+        }
+        pokemonHp.innerText = pokemon.stats[0].base_stat
+        pokemonAttack.innerText = pokemon.stats[1].base_stat
+        pokemonDefense.innerText = pokemon.stats[2].base_stat
+        pokemonSpecialAttack.innerText = pokemon.stats[3].base_stat
+        pokemonSpecialDefense.innerText = pokemon.stats[4].base_stat
+        pokemonSpeed.innerText = pokemon.stats[5].base_stat
+        pokemonImg.innerHTML = `<img id="sprite" src="${pokemon.sprites.front_default}" alt="pokemon-image" class="pokemonimage" />`
+    }

8cddd5f9836f615531b41ea420a3b48e586928d1
evilusean <82889521+evilusean@users.noreply.github.com>
2023-12-30 21:57:10 -0400

    updated tests to pass
    failed 70% of tests, need to check code

--- FCC-Javascript-Algos-DataStructures/2.0PalindromeCheck/script.js
+++ FCC-Javascript-Algos-DataStructures/2.0PalindromeCheck/script.js
@@ -1,5 +1,22 @@
-
+function palindrome(str) {
+    let strLowerCase = str.toLowerCase();
+    let editedStr = strLowerCase.replace(/[^0-9a-z]/gi, ''); 
+    let characters = editedStr.split("");
+    let charactersReversed = characters.slice(0).reverse();
+    let result = true;
+  
+    for (let i = 0; i < characters.length; i++){
+      if (characters[i] != charactersReversed[i]){
+        result = false;
+      }
+    }
+  
+    return result;
+  }
+//TO DO: ADD event listener on click, check text input 
+// return to where?
 const form = document.querySelector('form');
+  
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");

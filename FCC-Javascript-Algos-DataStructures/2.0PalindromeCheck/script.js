b195a20817db1f682225419d37b0351cc0ad4171
evilusean <82889521+evilusean@users.noreply.github.com>
2023-12-30 21:51:35 -0400

    Update script.js
    updated palindrome variable names and shorter palindrome function,

--- FCC-Javascript-Algos-DataStructures/2.0PalindromeCheck/script.js
+++ FCC-Javascript-Algos-DataStructures/2.0PalindromeCheck/script.js
@@ -1,34 +1,29 @@
-function palindrome(str) {
-    let strLowerCase = str.toLowerCase();
-    let editedStr = strLowerCase.replace(/[^0-9a-z]/gi, ''); 
-    let characters = editedStr.split("");
-    let charactersReversed = characters.slice(0).reverse();
-    let result = true;
-  
-    for (let i = 0; i < characters.length; i++){
-      if (characters[i] != charactersReversed[i]){
-        result = false;
-      }
-    }
-  
-    return result;
-  }
-//TO DO: ADD event listener on click, check text input 
-// return to where?
+
 const form = document.querySelector('form');
-  
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
 
-  const inputValue = document.querySelector('.text-input').value;
+checkBtn.addEventListener("click", () => {
+  const value = textInput.value;
+  const isPalindrome = palindrome(value);
 
-  const result2 = palindrome(inputValue);
-
-  const div = document.querySelector('result');
-
-  if (result2) {
-    div.innerHTML = 'The input is a palindrome!';
+  if (isPalindrome) {
+    result.innerHTML = `${value} is a palindrome`;
   } else {
-    div.innerHTML = 'The input is not a palindrome!';
+    result.innerHTML = `${value} is not a palindrome`;
   }
 });
+
+function palindrome(str) {
+  // Convert the string to lowercase and remove all non-alphanumeric characters.
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  // Check if the cleaned string is empty or has only one character.
+  if (cleanedStr === "" || cleanedStr.length === 1) {
+    return true;
+  }
+
+  // Compare the cleaned string to its reversed form.
+  return cleanedStr === cleanedStr.split("").reverse().join("");
+}

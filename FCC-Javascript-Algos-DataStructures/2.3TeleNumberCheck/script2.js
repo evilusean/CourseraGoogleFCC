03920ab00e9622bcc64a19279260b748b33df1da
evilusean <82889521+evilusean@users.noreply.github.com>
2024-01-01 00:50:34 -0400

    FAILED TESTS
    attempt 6, HOT GARBAGE

--- FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script2.js
+++ FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script2.js
@@ -33,11 +33,18 @@ form.addEventListener('click', (event) => {
 
   const userInput = document.getElementById('user-input').value;
 
-  if (userInput === '') {
-    alert('Please provide a phone number.');
-  } else {
-    const resultsDiv = document.getElementById('results-div');
-
+  const checkValidNumber = input => {
+    if (input === '') {
+      alert('Please provide a phone number');
+      return;
+    }
+    const countryCode = '^(1\\s?)?';
+    const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
+    const spacesDashes = '[\\s\\-]?';
+    const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
+    const phoneRegex = new RegExp(
+      `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
+    );
     const isValid = telephoneCheck(userInput);
 
     if (isValid) {

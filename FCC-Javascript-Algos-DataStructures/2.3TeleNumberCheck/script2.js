b05592cd4e01605cd55712743b9af952b00dab95
evilusean <82889521+evilusean@users.noreply.github.com>
2024-01-01 00:43:39 -0400

    Create Frankenscript
    creating frankenstein of all 5 attempts

---
+++ FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script2.js
@@ -0,0 +1,55 @@
+
+function telephoneCheck(str) {
+    const case1 = /^\d{3}-\d{3}-\d{4}$/; // 555-555-5555
+    const case2 = /^\(\d{3}\)\d{3}-\d{4}$/; // (555)555-5555
+    const case3 = /^\(\d{3}\)\s{1}\d{3}-\d{4}$/; // (555) 555-5555
+    const case4 = /^\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 555 555 5555
+    const case5 = /^\d{10}$/; // 5555555555
+    const case6 = /^1{1}\s{1}\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 1 555 55555555
+    const case7 = /^1{1}\s{1}\(\d{3}\)\s{1}\d{3}-\d{4}$/; // 1 (555)555-5555
+    const case8 = /^1{1}\(\d{3}\)\d{3}-\d{4}$/; // 1(555)555-5555
+    const case9 = /^1{1}\s{1}\d{3}-\d{3}-\d{4}$/; // 1 555-555-5555
+    const cases = [case1, case2, case3, case4, case5, case6, case7, case8, case9];
+    let caseIndex =0;
+  
+    while (caseIndex < cases.length){
+      if (cases[caseIndex].test(str) === false){
+        caseIndex +=1;
+      }
+  
+      else{
+        return true;
+      }
+    }
+  
+    return false;
+  }
+
+
+const form = document.querySelector('check-btn');
+
+form.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const userInput = document.getElementById('user-input').value;
+
+  if (userInput === '') {
+    alert('Please provide a phone number.');
+  } else {
+    const resultsDiv = document.getElementById('results-div');
+
+    const isValid = telephoneCheck(userInput);
+
+    if (isValid) {
+      resultsDiv.innerText = 'Valid US number: ' + userInput + '';
+    } else {
+      resultsDiv.innerText = 'Invalid US number: ' + userInput + '';
+    }
+  }
+});
+
+const clearBtn = document.getElementById('clear-btn');
+
+clearBtn.addEventListener('click', () => {
+  resultsDiv.innerHTML = '';
+});

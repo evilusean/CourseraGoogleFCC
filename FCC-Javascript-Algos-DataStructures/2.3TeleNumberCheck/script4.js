8da6e7758405706cd7141f2f0f12e2a4ad2c01f2
evilusean <82889521+evilusean@users.noreply.github.com>
2024-01-01 03:12:21 -0400

    INSEAN
    what is the definition of insanity?

--- FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script4.js
+++ FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script4.js
@@ -1,8 +1,15 @@
 //ATTEMPT 12 - The 'fun' never ends.
+/* THIS SHOULD NOT BE HARD: WHY CANT IT PASS A FCKING TEST
+When you click on the #check-btn element without entering a value into the 
+#user-input element, an alert should appear with the text Please provide a 
+phone number.
+*/
+
+const checkButton = document.querySelector("#check-btn");
 input = document.querySelector("input");
 checkButton.addEventListener("click", () => {
     // Get the phone number from the input field.
-    if (input.value === "" || null || undefined) {
+    if (input.value == "" || null || undefined) {
       alert("Please provide a phone number");
       return;
     }

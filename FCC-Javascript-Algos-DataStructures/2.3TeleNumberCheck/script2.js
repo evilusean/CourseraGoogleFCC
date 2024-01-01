ad917dff7586a59c8876980ea319fa4d88962ed4
evilusean <82889521+evilusean@users.noreply.github.com>
2024-01-01 01:09:16 -0400

    Update script2.js
    removed all that HOT TEST FAILING GARBAGE - starting fresh with a one liner

--- FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script2.js
+++ FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script2.js
@@ -1,48 +1,7 @@
+function numberChecker (str) {
 
-
-
-const form = document.querySelector('check-btn');
-
-form.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  const userInput = document.getElementById('user-input').value;
-
-const checkValidNumber = input => {
-  if (input === '') {
-    alert('Please provide a phone number');
-    return;
-  }
-  const countryCode = '^(1\\s?)?';
-  const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
-  const spacesDashes = '[\\s\\-]?';
-  const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
-  const phoneRegex = new RegExp(
-    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
-  );
-    const isValid = telephoneCheck(userInput);
-
-    if (isValid) {
-        const pTag = document.createElement('p');
-        pTag.className = 'results-text';
-        phoneRegex.test(input)
-          ? (pTag.style.color = '#00471b')
-          : (pTag.style.color = '#4d3800');
-        pTag.appendChild(
-          document.createTextNode(
-            `${phoneRegex.test(input) ? 'Valid' : 'Invalid'} US number: ${input}`
-          )
-        );
-        resultsDiv.appendChild(pTag);
-      } else {
-        resultsDiv.innerText = 'Invalid US number: ' + userInput + '';
-      }
+    let regex = /^1?\s?(\d{3}|\(\d{3}\))-?\s?\d{3}-?\s?\d{4}$/gm
+    
+    return regex.test(str);
+    
     }
-  });
-
-const clearBtn = document.getElementById('clear-btn');
-
-clearBtn.addEventListener('click', () => {
-  resultsDiv.innerHTML = '';
-});
-

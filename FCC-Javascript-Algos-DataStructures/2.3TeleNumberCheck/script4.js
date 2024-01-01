ea78f1f1ebb8c5d36c5e8cd0c3c942c62ddf888b
evilusean <82889521+evilusean@users.noreply.github.com>
2024-01-01 16:31:22 -0400

    Update script4.js
    and, it begins, another day, suffer.
    added in old code that "SHOULD" Work, and about to  create the exact same script AGAIN, for the nth time, if no progress after a few hours, going to the forums.

--- FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script4.js
+++ FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script4.js
@@ -1,76 +1,27 @@
-//ATTEMPT 12 - The 'fun' never ends.
-/* THIS SHOULD NOT BE HARD: WHY CANT IT PASS A FCKING TEST
-When you click on the #check-btn element without entering a value into the 
-#user-input element, an alert should appear with the text Please provide a 
-phone number.
-*/
-
-const checkBS = document.querySelector("#check-btn");
-const inputGarbage = document.querySelector("#user-input");
-checkBS.addEventListener("click", () => {
-    if (inputGarbage.value == "" || null || undefined || !inputGarbage.value || NaN) {
-      alert("Please provide a phone number");
-    }
-});
-
-// ATTEMPT 13 - If I don't get it fixed tomorrow, I'm asking for help on forums
+//ATTEMPT 14 - DAY 2 - Hour 15
 function telephoneCheck(str) {
-    const case1 = /^\d{3}-\d{3}-\d{4}$/; // 555-555-5555
-    const case2 = /^\(\d{3}\)\d{3}-\d{4}$/; // (555)555-5555
-    const case3 = /^\(\d{3}\)\s{1}\d{3}-\d{4}$/; // (555) 555-5555
-    const case4 = /^\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 555 555 5555
-    const case5 = /^\d{10}$/; // 5555555555
-    const case6 = /^1{1}\s{1}\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 1 555 55555555
-    const case7 = /^1{1}\s{1}\(\d{3}\)\s{1}\d{3}-\d{4}$/; // 1 (555)555-5555
-    const case8 = /^1{1}\(\d{3}\)\d{3}-\d{4}$/; // 1(555)555-5555
-    const case9 = /^1{1}\s{1}\d{3}-\d{3}-\d{4}$/; // 1 555-555-5555
-    const cases = [case1, case2, case3, case4, case5, case6, case7, case8, case9];
-    let caseIndex =0;
-  
-    while (caseIndex < cases.length){
-      if (cases[caseIndex].test(str) === false){
-        caseIndex +=1;
-      }
-  
-      else{
-        return true;
-      }
+  const num1 = /^\d{3}-\d{3}-\d{4}$/; // 555-555-5555
+  const num2 = /^\(\d{3}\)\d{3}-\d{4}$/; // (555)555-5555
+  const num3 = /^\(\d{3}\)\s{1}\d{3}-\d{4}$/; // (555) 555-5555
+  const num4 = /^\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 555 555 5555
+  const num5 = /^\d{10}$/; // 5555555555
+  const num6 = /^1{1}\s{1}\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 1 555 55555555
+  const num7 = /^1{1}\s{1}\(\d{3}\)\s{1}\d{3}-\d{4}$/; // 1 (555)555-5555
+  const num8 = /^1{1}\(\d{3}\)\d{3}-\d{4}$/; // 1(555)555-5555
+  const num9 = /^1{1}\s{1}\d{3}-\d{3}-\d{4}$/; // 1 555-555-5555
+  const nums = [num1, num2, num3, num4, num5, num6, num7, num8, num9];
+  let numIndex =0;
+
+  while (numIndex < nums.length){
+    if (nums[numIndex].test(str) === false){
+      numIndex +=1;
     }
-  
-    return false;
-  }
 
-
-const form = document.querySelector('check-btn');
-
-form.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  const userInput = document.getElementById('user-input').value;
-
-  if (userInput === '') {
-    alert('Please provide a phone number.');
-  } else {
-    const resultsDiv = document.getElementById('results-div');
-
-    const isValid = telephoneCheck(userInput);
-
-    if (isValid) {
-      resultsDiv.innerText = 'Valid US number: ' + userInput + '';
-    } else {
-      resultsDiv.innerText = 'Invalid US number: ' + userInput + '';
+    else{
+      return true;
     }
   }
-});
 
-const clearBtn = document.getElementById('clear-btn');
-
-clearBtn.addEventListener('click', () => {
-  resultsDiv.innerHTML = '';
-});
-
-function isValidPhoneNumber(phoneNumber) {
-const phoneNumberRegex = /^(\d{3})-(\d{3})-(\d{4})$/;
-
-return phoneNumberRegex.test(phoneNumber);
+  return false;
 }
+

c5530c4abd116853b283592fcafbb150e7985b94
evilusean <82889521+evilusean@users.noreply.github.com>
2023-12-31 22:44:43 -0400

    Update script.js
    still not passing tests, minor changes

--- FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script.js
+++ FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script.js
@@ -4,8 +4,8 @@ function telephoneCheck(str) {
     const case3 = /^\(\d{3}\)\s{1}\d{3}-\d{4}$/; // (555) 555-5555
     const case4 = /^\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 555 555 5555
     const case5 = /^\d{10}$/; // 5555555555
-    const case6 = /^1{1}\s{1}\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 1 555 555 5555
-    const case7 = /^1{1}\s{1}\(\d{3}\)\s{1}\d{3}-\d{4}$/; // 1 (555) 555-5555
+    const case6 = /^1{1}\s{1}\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 1 555 55555555
+    const case7 = /^1{1}\s{1}\(\d{3}\)\s{1}\d{3}-\d{4}$/; // 1 (555)555-5555
     const case8 = /^1{1}\(\d{3}\)\d{3}-\d{4}$/; // 1(555)555-5555
     const case9 = /^1{1}\s{1}\d{3}-\d{3}-\d{4}$/; // 1 555-555-5555
     const cases = [case1, case2, case3, case4, case5, case6, case7, case8, case9];
@@ -13,7 +13,7 @@ function telephoneCheck(str) {
   
     while (caseIndex < cases.length){
       if (cases[caseIndex].test(str) === false){
-        caseIndex += 1;
+        caseIndex +=1;
       }
   
       else{
@@ -39,9 +39,9 @@ function telephoneCheck(str) {
       const isValid = telephoneCheck(userInput);
   
       if (isValid) {
-        resultsDiv.innerHTML = 'Valid US number: ' + userInput + '';
+        resultsDiv.innerText = 'Valid US number: ' + userInput + '';
       } else {
-        resultsDiv.innerHTML = 'Invalid US number: ' + userInput + '';
+        resultsDiv.innerText = 'Invalid US number: ' + userInput + '';
       }
     }
   });

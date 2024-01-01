d6a057a13937126754d5f358a6b2016cf56ddd05
evilusean <82889521+evilusean@users.noreply.github.com>
2024-01-01 00:59:10 -0400

    Update script2.js
    Whatever is the problem, either JS or Regex, I hate, When I figure out wtf is wrong with this code, I will eternally cement my hatred of either regex, testing, or JS - attempt 7?

--- FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script2.js
+++ FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script2.js
@@ -1,29 +1,4 @@
 
-function telephoneCheck(str) {
-    const case1 = /^\d{3}-\d{3}-\d{4}$/; // 555-555-5555
-    const case2 = /^\(\d{3}\)\d{3}-\d{4}$/; // (555)555-5555
-    const case3 = /^\(\d{3}\)\s{1}\d{3}-\d{4}$/; // (555) 555-5555
-    const case4 = /^\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 555 555 5555
-    const case5 = /^\d{10}$/; // 5555555555
-    const case6 = /^1{1}\s{1}\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 1 555 55555555
-    const case7 = /^1{1}\s{1}\(\d{3}\)\s{1}\d{3}-\d{4}$/; // 1 (555)555-5555
-    const case8 = /^1{1}\(\d{3}\)\d{3}-\d{4}$/; // 1(555)555-5555
-    const case9 = /^1{1}\s{1}\d{3}-\d{3}-\d{4}$/; // 1 555-555-5555
-    const cases = [case1, case2, case3, case4, case5, case6, case7, case8, case9];
-    let caseIndex =0;
-  
-    while (caseIndex < cases.length){
-      if (cases[caseIndex].test(str) === false){
-        caseIndex +=1;
-      }
-  
-      else{
-        return true;
-      }
-    }
-  
-    return false;
-  }
 
 
 const form = document.querySelector('check-btn');
@@ -33,18 +8,18 @@ form.addEventListener('click', (event) => {
 
   const userInput = document.getElementById('user-input').value;
 
-  const checkValidNumber = input => {
-    if (input === '') {
-      alert('Please provide a phone number');
-      return;
-    }
-    const countryCode = '^(1\\s?)?';
-    const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
-    const spacesDashes = '[\\s\\-]?';
-    const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
-    const phoneRegex = new RegExp(
-      `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
-    );
+const checkValidNumber = input => {
+  if (input === '') {
+    alert('Please provide a phone number');
+    return;
+  }
+  const countryCode = '^(1\\s?)?';
+  const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
+  const spacesDashes = '[\\s\\-]?';
+  const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
+  const phoneRegex = new RegExp(
+    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
+  );
     const isValid = telephoneCheck(userInput);
 
     if (isValid) {

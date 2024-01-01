ce985e1dea5aafaf5f3712db8939a33bf89e3c4f
evilusean <82889521+evilusean@users.noreply.github.com>
2024-01-01 19:22:28 -0400

    Progress!
    all tests passed except 1, such a simple fix, for the <script> tag, it needs to be placed AFTER the DOM you are trying to select or you need to use the async arg in the < script> tag, it SHOULD work, and DOES, FINALLY :) thank you bbsmooth, random stranger from forum online that gave me the critical advice

--- FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script4.js
+++ FCC-Javascript-Algos-DataStructures/2.3TeleNumberCheck/script4.js
@@ -1,51 +1,50 @@
-//ATTEMPT 14 - DAY 2 - Hour 15
-function telephoneCheck(str) {
-  const num1 = /^\d{3}-\d{3}-\d{4}$/; // 555-555-5555
-  const num2 = /^\(\d{3}\)\d{3}-\d{4}$/; // (555)555-5555
-  const num3 = /^\(\d{3}\)\s{1}\d{3}-\d{4}$/; // (555) 555-5555
-  const num4 = /^\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 555 555 5555
-  const num5 = /^\d{10}$/; // 5555555555
-  const num6 = /^1{1}\s{1}\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 1 555 55555555
-  const num7 = /^1{1}\s{1}\(\d{3}\)\s{1}\d{3}-\d{4}$/; // 1 (555)555-5555
-  const num8 = /^1{1}\(\d{3}\)\d{3}-\d{4}$/; // 1(555)555-5555
-  const num9 = /^1{1}\s{1}\d{3}-\d{3}-\d{4}$/; // 1 555-555-5555
-  const nums = [num1, num2, num3, num4, num5, num6, num7, num8, num9];
-  let numIndex =0;
-  while (numIndex < nums.length){
-    if (nums[numIndex].test(str) === false){
-      numIndex +=1;
+  //ATTEMPT 14 - DAY 2 - Hour 15
+  function telephoneCheck(str) {
+    const num1 = /^\d{3}-\d{3}-\d{4}$/; // 555-555-5555
+    const num2 = /^\(\d{3}\)\d{3}-\d{4}$/; // (555)555-5555
+    const num3 = /^\(\d{3}\)\s{1}\d{3}-\d{4}$/; // (555) 555-5555
+    const num4 = /^\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 555 555 5555
+    const num5 = /^\d{10}$/; // 5555555555
+    const num6 = /^1{1}\s{1}\d{3}\s{1}\d{3}\s{1}\d{4}$/; // 1 555 55555555
+    const num7 = /^1{1}\s{1}\(\d{3}\)\s{1}\d{3}-\d{4}$/; // 1 (555)555-5555
+    const num8 = /^1{1}\(\d{3}\)\d{3}-\d{4}$/; // 1(555)555-5555
+    const num9 = /^1{1}\s{1}\d{3}-\d{3}-\d{4}$/; // 1 555-555-5555
+    const nums = [num1, num2, num3, num4, num5, num6, num7, num8, num9];
+    let numIndex =0;
+    while (numIndex < nums.length){
+      if (nums[numIndex].test(str) === false){
+        numIndex +=1;
+      }
+      else{
+        return true;
+      }
     }
-    else{
-      return true;
-    }
+    return false;
   }
-  return false;
-}
 
-const form = document.querySelector('check-btn');
+  const form = document.getElementById('check-btn');
 
-form.addEventListener('click', (event) => {
-  event.preventDefault();
+  form.addEventListener('click', () => {
 
-  const userInput = document.getElementById('user-input').value;
+    const userInput = document.getElementById('user-input').value;
 
-  if (userInput === '') {
-    alert('Please provide a phone number');
-  } else {
-    const resultsDiv = document.getElementById('results-div');
-
-    const isValid = telephoneCheck(userInput);
-
-    if (isValid) {
-      resultsDiv.innerText = 'Valid US number: ' + userInput;
+    if (userInput === '') {
+      alert('Please provide a phone number');
     } else {
-      resultsDiv.innerText = 'Invalid US number: ' + userInput;
+      const resultsDiv = document.getElementById('results-div');
+
+      const isValid = telephoneCheck(userInput);
+
+      if (isValid) {
+        resultsDiv.innerHTML = 'Valid US number: ' + userInput;
+      } else {
+        resultsDiv.innerHTML = 'Invalid US number: ' + userInput;
+      }
     }
-  }
-});
+  });
 
-const clearBtn = document.getElementById('clear-btn');
+  const clearBtn = document.getElementById('clear-btn');
 
-clearBtn.addEventListener('click', () => {
-  resultsDiv.innerHTML = '';
-});
+  clearBtn.addEventListener('click', () => {
+    resultsDiv.innerHTML = '';
+  });

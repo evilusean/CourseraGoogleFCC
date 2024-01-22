48dd1966e9434f417a5f6e870719337adf6d9df2
evilusean <82889521+evilusean@users.noreply.github.com>
2024-01-22 18:27:06 -0400

    Create learn-about-self-closing-jsx-tags.jsx

---
+++ FCC-React-Redux-SPA-Bootstrap-Front-End-Development-Libraries/1.4React/learn-about-self-closing-jsx-tags.jsx
@@ -0,0 +1,9 @@
+const JSX = (
+<div>
+  {/* change code below this line */}
+  <h2>Welcome to React!</h2> <br />
+  <p>Be sure to close all tags!</p>
+  <hr />
+  {/* change code above this line */}
+</div>
+);

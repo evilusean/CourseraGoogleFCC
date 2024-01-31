93ef6ee604a6ee123c06072c0f9d82ed489ff1b2
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-01-31 14:19:58 -0400

    Create map-dispatch-to-props.jsx

---
+++ FCC-React-Redux-SPA-Bootstrap-Front-End-Development-Libraries/1.6React-Redux/map-dispatch-to-props.jsx
@@ -0,0 +1,16 @@
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+// change code below this line
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: function(message) {
+      dispatch(addMessage(message));
+    }
+  }
+};

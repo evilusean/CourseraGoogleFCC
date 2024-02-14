b77523e92ee8c7710e358b0cb9469917db4b96e5
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-02-14 15:15:33 -0400

    Create Variables.php
    created variables examples

---
+++ FCC-PHP/Variables.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Website</title>
+</head>
+<body>
+  <?php
+  $age = 30;
+  $name = "Sean";
+    echo "There once was a man named $name <br>";
+    echo "He was $age years old <br>";
+    echo "He really liked the name $name <br>";
+    echo "But didn't like being $age <br>"
+  ?>
+</body>
+</html>

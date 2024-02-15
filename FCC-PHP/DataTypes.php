1711fb003bcb2deac2cbed1a1b2171cc72718ea0
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-02-14 20:49:31 -0400

    Update DataTypes.php
    added echo and text to view in browser

--- FCC-PHP/DataTypes.php
+++ FCC-PHP/DataTypes.php
@@ -9,7 +9,12 @@
     $integer = 30;
     $float = 1.1235813;
     $boolean = true;
-    null
+    null;
+    echo "$string <br>";
+    echo "This is an integer: $integer <br>";
+    echo "This is a float: $float <br>";
+    echo "This is a boolean: $boolean <br>";
+    echo null
   ?>
 </body>
 </html>

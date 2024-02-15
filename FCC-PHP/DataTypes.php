f16e1d8c98785109317a75719f47e29251f1cc35
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-02-14 20:35:40 -0400

    created datatypes.php

---
+++ FCC-PHP/DataTypes.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>DataTypes</title>
+</head>
+<body>
+  <?php
+    $string = "This is a String";
+    $integer = 30;
+    $float = 1.1235813;
+    $boolean = true;
+    null
+  ?>
+</body>
+</html>
--- FCC-PHP/Variables.php
+++ FCC-PHP/Variables.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>My Website</title>
+  <title>Variables</title>
 </head>
 <body>
   <?php

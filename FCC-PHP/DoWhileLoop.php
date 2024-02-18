f3252a9c7df1e06a88f7b927cafde2c921664971
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-02-18 02:29:01 -0400

    Create DoWhileLoop.php

---
+++ FCC-PHP/DoWhileLoop.php
@@ -0,0 +1,15 @@
+<!DOCTYPE php>
+<html>
+<head>
+  <title>Do While Loops</title>
+</head>
+<body>
+<?php 
+      $index = 6;
+      do {
+        echo "$index <br>";
+        $index++;
+      } while ($index <= 5)
+    ?>
+</body>
+</html>

fe038d033308bb4f1e8165032d2b43559ed592c4
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-02-18 02:24:09 -0400

    Create 2.8WhileLoop.php

---
+++ FCC-PHP/2.8WhileLoop.php
@@ -0,0 +1,15 @@
+<!DOCTYPE php>
+<html>
+<head>
+  <title>While Loops</title>
+</head>
+<body>
+    <?php 
+      $index = 1;
+      while ($index <= 5){
+        echo "$index <br>";
+        $index++;
+      }
+    ?>
+</body>
+</html>

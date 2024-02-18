de46b2fedeed90af00edf74631ca39f0d4e27b0e
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-02-18 15:38:36 -0400

    Create 3.0ForLoopExample.php

---
+++ FCC-PHP/3.0ForLoopExample.php
@@ -0,0 +1,13 @@
+<!DOCTYPE php>
+<html>
+<head>
+  <title>While Loops</title>
+</head>
+<body>
+    <?php 
+      for ($index = 1; $index <= 5; $index++){
+        echo "$index <br>";
+      }
+    ?>
+</body>
+</html>

e11c0677e9ba6c2aceae9d8f57d8ec40333a6f52
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-02-25 05:41:06 -0400

    Update app.blade.php
    copied some code, still won't load the css....

--- FCC-PHP-Laravel/freeCodeGram/resources/views/layouts/app.blade.php
+++ FCC-PHP-Laravel/freeCodeGram/resources/views/layouts/app.blade.php
@@ -18,16 +18,18 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand d-flex" href="{{ url('/') }}">
-                    <div><img src="/svg/freeCodeCampLogo.svg" style="height: 25px;"></div>
-                    <div>freeCodeGram</div>
+                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                    <div><img src="/svg/freeCodeCampLogo.svg" style="height: 20px; border-right: 1px solid #333;" class="pr-3"></div>
+                    <div class="pl-3">freeCodeGram</div>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 

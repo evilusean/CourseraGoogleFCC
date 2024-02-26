94298b22ddea1691879d84cf17c46a7c8955f349
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-02-26 07:44:40 -0400

    Update home.blade.php
    added posts/followers/following

--- FCC-PHP-Laravel/freeCodeGram/resources/views/home.blade.php
+++ FCC-PHP-Laravel/freeCodeGram/resources/views/home.blade.php
@@ -3,11 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3">
+        <div class="col-3 p-5">
             <img src="https://github.com/evilusean/DaGram-InstagramClone/blob/main/Images/NP%20Seans/ChadNpc.png?raw=true" class="rounded-circle" style="width: 150px";></img>
         </div>
-        <div class="col-9">
-        
+        <div class="col-9 pt-5">
+            <div>
+                <h1>freeCodeGram</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pe-5">
+                    <strong>153</strong> posts
+                </div>
+                <div class="pe-5">
+                    <strong>23k</strong> followers
+                </div>
+                <div class="pe-5">
+                    <strong>212</strong> following
+                </div>
+            </div>
         </div>
     </div>
 </div>

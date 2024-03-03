475e4213eccf9742b1388b54844bf04302e52324
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-03 04:23:05 -0400

    Update PostsController.php
    testing our new create post with dd()

--- FCC-PHP-Laravel/freeCodeGram/app/Http/Controllers/PostsController.php
+++ FCC-PHP-Laravel/freeCodeGram/app/Http/Controllers/PostsController.php
@@ -10,4 +10,21 @@ class PostsController extends Controller
     {
         return view('posts.create');
     }
+    public function store()
+    {
+        // Create a new post using the request data)
+        dd(request()->all());
+        
+        // Validate the data
+        $this->validate(request(), [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        // Create a new post using the request data
+        auth()->user()->publish(
+            new Post(request(['title', 'body']))
+        );
+        // Redirect to the home page
+        return redirect('/');
+    }
 }

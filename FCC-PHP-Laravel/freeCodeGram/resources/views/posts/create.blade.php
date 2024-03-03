8c5581039b7525309027c55b1b1572eeba9af857
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-03 04:14:07 -0400

    Update create.blade.php
    added image label, input form, and error message copied from above

--- FCC-PHP-Laravel/freeCodeGram/resources/views/posts/create.blade.php
+++ FCC-PHP-Laravel/freeCodeGram/resources/views/posts/create.blade.php
@@ -22,6 +22,16 @@
                             </span>
                     @enderror
                 </div>
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                    <input type="file" class="form-control-file" id="image" name="image">
+                    @error('image')
+                            <strong>{{ $message }}</strong>
+                    @enderror
+                </div>
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Add New Post</button>
+                </div>
             </div>
         </div>
     </form>

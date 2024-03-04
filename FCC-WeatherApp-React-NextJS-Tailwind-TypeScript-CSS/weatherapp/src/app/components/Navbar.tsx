93549a28ebf9e94e782101c18f4b1294685e075e
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-04 07:12:14 -0400

    added sun react-icons
    added react-icons through command line and added a sun with import, and styled with tailwind to make it yellow

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/components/Navbar.tsx
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BiSolidSun } from "react-icons/bi"; 
 
 type Props = {}
 
@@ -6,10 +7,11 @@ export default function Navbar({}: Props) {
     return (
         <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
             <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
-                <p className="flex items-center justify-center gap-2"></p>
-                <h2 className="text-gray-500 text-3x1">Weather</h2>
+                <p className="flex items-center justify-center gap-2">
+                    <h2 className="text-gray-500 text-3x1">Weather</h2>
+                    <BiSolidSun className="text-3x1 mt-1 text-yellow-500"/>
+                </p>
             </div>
-            <h1>Navbar</h1>
         </nav>
     )
 }

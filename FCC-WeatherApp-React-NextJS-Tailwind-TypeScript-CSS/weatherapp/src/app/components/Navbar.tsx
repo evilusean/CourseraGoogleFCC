ef3d7fbf069459fe283fcfe4dfb3363f61dcb1e0
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-04 07:38:33 -0400

    Update Navbar.tsx
    added 2 more icons and changed sun icon

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/components/Navbar.tsx
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { BiSolidSun } from "react-icons/bi"; 
+import { MdWbSunny } from "react-icons/md";
+import { MdMyLocation } from "react-icons/md";
+import { MdOutlineLocationOn } from "react-icons/md";
+
 
 type Props = {}
 
@@ -7,10 +10,14 @@ export default function Navbar({}: Props) {
     return (
         <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
             <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
-                <p className="flex items-center justify-center gap-2">
+                <div className="flex items-center justify-center gap-2">
                     <h2 className="text-gray-500 text-3x1">Weather</h2>
-                    <BiSolidSun className="text-3x1 mt-1 text-yellow-500"/>
-                </p>
+                    <MdWbSunny className="text-3x1 mt-1 text-yellow-500"/>
+                </div>
+                <section className="flex gap-2 items-center">
+                    <MdMyLocation className='text-3x1 text-gray-400 hover:opacity-80 cursor-pointer' />
+                    <MdOutlineLocationOn className='text-3x1 text-gray-400 hover:opacity-80 cursor-pointer' />
+                </section>
             </div>
         </nav>
     )

01541dc4ee7e41ae582a6884f8ea09e6248560c1
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-05 09:26:18 -0400

    npm i date-fns
    adding a new section to show the current date and time from our data fetched by API DateA

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/page.tsx
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/page.tsx
@@ -93,6 +93,23 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7x1 mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/* TODAYS DATA */}
+        <section>
+          <div>
+            <h2 className="flex gap-1 text-2x1 items-end">
+              <p></p>
+              
+            </h2>
+            <div></div>
+          </div>
+
+        </section>
+        {/* 7 DAY FORECAST */}
+        <section>
+          
+        </section>
+      </main>
     </div>
   );
 }

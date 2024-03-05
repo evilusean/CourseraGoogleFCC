cba91a31a9f10418acb0bbb79b7f011f3ae13800
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-05 02:19:15 -0400

    Update page.tsx
    added API key for testing

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/page.tsx
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 
+// Below Boilerplate for API Key:
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// Below Working Copy for API Key NEXT_PUBLIC_API_KEY found in .env.local file:
+// http://api.openweathermap.org/geo/1.0/direct?q=Tokyo&appid={process.env.NEXT_PUBLIC_API_KEY}
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">

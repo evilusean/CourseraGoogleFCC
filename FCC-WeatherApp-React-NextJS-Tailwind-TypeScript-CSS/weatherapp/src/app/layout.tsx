47e6520467ad42085b3a14f9701e531f6fd0be50
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-05 07:52:24 -0400

    npm i react-query
    installed react-query with boilerplate, wrapped body in a <QueryClientProvider> of layout.tsx

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/layout.tsx
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +15,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient()
   return (
     <html lang="en">
+      <QueryClientProvider client={queryClient}>
       <body className={inter.className}>{children}</body>
+      </QueryClientProvider>
     </html>
   );
 }

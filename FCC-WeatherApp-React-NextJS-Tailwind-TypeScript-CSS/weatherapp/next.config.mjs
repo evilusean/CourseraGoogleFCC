8f1c33fcd1f77eeffe6a8cd88dbc72ee3d670c03
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-07 09:48:42 -0400

    Update next.config.mjs
    something broke, need to fix

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/next.config.mjs
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'openweathermap.org',
+        },
+      ],
+    },
+  }

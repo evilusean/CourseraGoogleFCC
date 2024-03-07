e225fcb2184cbe27046ab8e4524bb89b78e6505c
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-07 05:25:30 -0400

    Update page.tsx
    tested code with a URL api fetch, it worked, I think I found the problem

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/page.tsx
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
       async () =>
     {
       const {data} = await axios.get(
-      'http://api.openweathermap.org/data/2.5/forecast?q=Tokyo,JP&appid=${process.env.NEXT_PUBLIC_API_KEY}=56');
+      'http://api.openweathermap.org/data/2.5/forecast?q=Tokyo,JP&appid=${process.env.NEXT_PUBLIC_API_KEY}&cnt=56');
       return data;
     }
       //OLD METHOD: if you are using axios, you don't need to use fetch, or convert it into a json, with axios it will do it automatically

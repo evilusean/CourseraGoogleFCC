44040df6984b54efd57a484b51595a056b53e93b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-08 05:08:15 -0400

    meters to kilometers function
    adding in API fetches to our properties, created a new function for meters to KM

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/page.tsx
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/page.tsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { format, parseISO } from "date-fns";
+import { format, fromUnixTime, parseISO } from "date-fns";
 import Container from "./components/container";
 import { convertKelvinToCelsius } from "./utils/convertKelvinToCelsius";
 import WeatherIcon from "./components/WeatherIcon";
 import { getDayOrNightIcon } from "./utils/getDayOrNightIcon";
 import WeatherDetails from "./components/WeatherDetails";
+import { meterToKilometer } from "./utils/metersToKilometers";
 
 //const WEATHER_API_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY;
 //console.log("WEATHER_API_KEY", WEATHER_API_KEY);
@@ -83,7 +84,7 @@ export default function Home() {
       async () =>
     {
       const {data} = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=Tokyo&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`);
+        //`https://api.openweathermap.org/data/2.5/forecast?q=Tokyo&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`);
       return data;
     });
       //OLD METHOD: if you are using axios, you don't need to use fetch, or convert it into a json, with axios it will do it automatically
@@ -169,7 +170,23 @@ export default function Home() {
                   />
             </Container>
             <Container className="bg-yellow-300/80 px-6 gap-4 justify-between overflow-x-auto">
-                <WeatherDetails />
+                <WeatherDetails visibility={meterToKilometer(firstData?.visibility ?? 10000)}
+                airPressure={'${firstData?.main.pressure} hPa'}
+                humidity={'${firstData?.main.humidity}%'} 
+                sunrise={format(
+                  fromUnixTime(data?.city.sunrise ?? 1702949452), 
+                  'H:mm'
+                  )} 
+                sunset={format(
+                  fromUnixTime(data?.city.sunset ?? 1702949452), 
+                  'H:mm'
+                  )}
+                windSpeed={}
+                />
+
+            </Container>
+          </div>
+        </section/>
             </Container>
           </div>
         </section>
---
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/utils/metersToKilometers.ts
@@ -0,0 +1,4 @@
+export function meterToKilometer(visibilityInMeters: number): string {
+  const visibilityInKilometers = visibilityInMeters / 1000;
+  return `${visibilityInKilometers.toFixed(0)}km`;
+}

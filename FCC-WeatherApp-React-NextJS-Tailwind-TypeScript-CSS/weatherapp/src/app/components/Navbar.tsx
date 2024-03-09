87ebb2e152626b858ef0fd122c893d3c3fc70e7b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-09 08:54:12 -0400

    Update Navbar.tsx
    created try catch block of code and added several states for exceptions and errors

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/components/Navbar.tsx
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/components/Navbar.tsx
@@ -1,13 +1,46 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import { MdWbSunny } from "react-icons/md";
 import { MdMyLocation } from "react-icons/md";
 import { MdOutlineLocationOn } from "react-icons/md";
 import SearchBox from './SearchBox';
+import axios from 'axios';
 
 
 type Props = {}
 
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY;
+
 export default function Navbar({}: Props) {
+    const [city, setCity] = useState("");
+    const [error, setError] = useState("");
+
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+    const [showSiggestions, setShowSuggestions] = useState(false);
+
+    async function handInputChange(value: string) {
+        setCity(value);
+        if (value.length >= 3) {
+            try {
+                const response = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=${API_KEY}&cnt=56`
+                );
+                const suggestions = response.data.list.map((item:any)=>item.name); //will store all suggestions in a variable
+                setSuggestions(suggestions);
+                setError('');
+                setShowSuggestions(true);
+            } catch (error) {
+                setSuggestions([])
+                setShowSuggestions(false);
+            }   
+        }
+        else {
+            setSuggestions([])
+            setShowSuggestions(false);
+        }
+    }
+
     return (
         <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
             <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
@@ -20,10 +53,18 @@ export default function Navbar({}: Props) {
                     <MdOutlineLocationOn className='text-3x1 text-gray-400 hover:opacity-80 cursor-pointer' />
                     <p className="text-slate-900/80 text-sm">  Failed State canada  </p>
                     <div>
-                        <SearchBox />
+                        <SearchBox 
+                            value={city}
+                            //onSubmit={}
+                            onChange = {(e)=>handInputChange(e.target.value)}
+                        />
                     </div>
                 </section>
             </div>
         </nav>
     )
+}
+
+function SuggestionBox(){
+    return <ul className="mb-4"><li></li></ul>
 }

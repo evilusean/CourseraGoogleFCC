c72ab4cd2728995f503f70f2ffd4b2d803308a91
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-09 11:06:02 -0400

    Update Navbar.tsx
    fixed formatting

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/components/Navbar.tsx
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/components/Navbar.tsx
@@ -78,12 +78,23 @@ function SuggestionBox({
     error: string;
 }){
     return (
-    <>
-        { ((showSuggestions && suggestions.length >1) || error) && (
-            <ul className="mb-4 bg-white gap-1 py-2 px-2 flex flex-col absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px]">
-                <li className="cursor-pointer p-1 rounded hover:bg-gray-200"></li>
+        <>
+          {((showSuggestions && suggestions.length > 1) || error) && (
+            <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px]  flex flex-col gap-1 py-2 px-2">
+              {error && suggestions.length < 1 && (
+                <li className="text-red-500 p-1 "> {error}</li>
+              )}
+              {suggestions.map((item, i) => (
+                <li
+                  key={i}
+                  onClick={() => handleSuggestionClick(item)}
+                  className="cursor-pointer p-1 rounded   hover:bg-gray-200"
+                >
+                  {item}
+                </li>
+              ))}
             </ul>
-        )}
-    </>
-    );
-}
+          )}
+        </>
+      );
+    }

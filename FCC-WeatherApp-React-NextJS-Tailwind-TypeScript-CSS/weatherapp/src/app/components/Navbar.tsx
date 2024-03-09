0ebc0112f07afc1bdd50f592718cb6bad53f9634
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-09 11:12:08 -0400

    Update Navbar.tsx
    added props and handle clicks

--- FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/components/Navbar.tsx
+++ FCC-WeatherApp-React-NextJS-Tailwind-TypeScript-CSS/weatherapp/src/app/components/Navbar.tsx
@@ -41,6 +41,11 @@ export default function Navbar({}: Props) {
         }
     }
 
+    function handleSuggestionClick(value: string) {
+        setCity(value);
+        setShowSuggestions(false);
+    }
+
     return (
         <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
             <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
@@ -58,7 +63,14 @@ export default function Navbar({}: Props) {
                             //onSubmit={}
                             onChange = {(e)=>handInputChange(e.target.value)}
                         />
-                        <SuggestionBox />
+                        <SuggestionBox 
+                            {...{
+                                showSuggestions,
+                                suggestions,
+                                handleSuggestionClick,
+                                error
+                            }} 
+                        />
                     </div>
                 </section>
             </div>
@@ -80,7 +92,7 @@ function SuggestionBox({
     return (
         <>
           {((showSuggestions && suggestions.length > 1) || error) && (
-            <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px]  flex flex-col gap-1 py-2 px-2">
+            <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2">
               {error && suggestions.length < 1 && (
                 <li className="text-red-500 p-1 "> {error}</li>
               )}
@@ -88,7 +100,7 @@ function SuggestionBox({
                 <li
                   key={i}
                   onClick={() => handleSuggestionClick(item)}
-                  className="cursor-pointer p-1 rounded   hover:bg-gray-200"
+                  className="cursor-pointer p-1 rounded hover:bg-gray-200"
                 >
                   {item}
                 </li>

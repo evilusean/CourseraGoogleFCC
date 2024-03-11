9a5aea9de8995b4c5f2dd36c67eda020a3f1897f
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-11 12:26:17 -0300

    Update layout.js
    fixed bug

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/layout.js
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/layout.js
@@ -2,7 +2,7 @@ import Nav from "./(components)/Nav";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
-import config from "@fortawesome/fontawesome-svg-core";
+import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 config.autoAddCss = false;

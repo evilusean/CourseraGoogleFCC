c9ef36b6edc4cb5d8607de70d9daaf1a398a33ed
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-12 10:59:20 -0300

    Update DeleteBlock.jsx
    added icon

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/DeleteBlock.jsx
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/DeleteBlock.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const DeleteBlock = () => {
-  return <div>DeleteBlock</div>;
+  return (
+    <FontAwesomeIcon
+      icon={faX}
+      className="text-red-400 hover:cursor-pointer hover:text-red-200"
+    />
+  );
 };
 
 export default DeleteBlock;

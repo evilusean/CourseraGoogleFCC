36b48e44176899b75c3c879f547bf0f3b93e12b0
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-12 11:12:12 -0300

    added PriorityDisplay.jsx
    Fire

---
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/PriorityDisplay.jsx
@@ -0,0 +1,16 @@
+import { faFire } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const PriorityDisplay = () => {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faFire} />
+      <FontAwesomeIcon icon={faFire} />
+      <FontAwesomeIcon icon={faFire} />
+      <FontAwesomeIcon icon={faFire} />
+      <FontAwesomeIcon icon={faFire} />
+    </div>
+  );
+};
+
+export default PriorityDisplay;
--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/page.jsx
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/page.jsx
@@ -5,6 +5,8 @@ const Dashboard = () => {
   return (
     <div>
       <TicketCard />
+      <TicketCard />
+      <TicketCard />
     </div>
   );
 };

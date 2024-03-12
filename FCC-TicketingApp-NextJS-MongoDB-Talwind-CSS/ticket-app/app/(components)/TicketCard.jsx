2ec03d0289584137fbe68a58c23e03d316afae46
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-12 13:30:13 -0300

    Update TicketCard.jsx
    better formatting

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketCard.jsx
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketCard.jsx
@@ -5,9 +5,13 @@ import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
-      <PriorityDisplay />
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
       <ProgressDisplay />
       <StatusDisplay />
     </div>

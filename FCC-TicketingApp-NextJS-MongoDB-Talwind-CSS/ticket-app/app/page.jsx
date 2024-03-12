f05790ab0c184b4d11519f48835c7f8029fe15a0
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-12 11:01:54 -0300

    created ticketCard.jsx
    created TicketCard.jsx component, added to dashboard page.jsx

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/page.jsx
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TicketCard from "./(components)/TicketCard";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <TicketCard />
+    </div>
+  );
 };
 
 export default Dashboard;

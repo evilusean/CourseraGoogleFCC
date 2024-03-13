dee1f2d743d370cfa33767c4f762e860e2678f63
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-13 15:01:40 -0300

    Update TicketForm.jsx
    added last4 radio buttons for priority

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketForm.jsx
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketForm.jsx
@@ -76,6 +76,42 @@ const TicketForm = () => {
             checked={formData.priority == 1}
           />
           <label>1</label>
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
         </div>
       </form>
     </div>

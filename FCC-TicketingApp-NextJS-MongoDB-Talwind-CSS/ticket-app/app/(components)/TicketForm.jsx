e041b6f250a508bd0ab43cfa2fe3e567e155f96f
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-13 15:06:20 -0300

    Update TicketForm.jsx
    added status and submit

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketForm.jsx
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketForm.jsx
@@ -124,6 +124,12 @@ const TicketForm = () => {
           onChange={handleChange}
         />
         <label>Status</label>
+        <select name="status" value={formData.status} onChange={handleChange}>
+          <option value="not started">Not Started</option>
+          <option value="started">Started</option>
+          <option value="done">Done</option>
+        </select>
+        <input type="submit" className="btn" value="Create Ticket" />
       </form>
     </div>
   );

6049e2bf577124f1cd1104f529b802f0cb44e59b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-13 10:29:14 -0300

    Create TicketForm.jsx
    creating our first form input

---
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketForm.jsx
@@ -0,0 +1,35 @@
+"in client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const TicketForm = () => {
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+    category: "Hardware Problem",
+  };
+
+  const [formData, setForm] = useState(startingTicketData);
+  return (
+    <div className="flex justify-center">
+      <form>
+        <h3>Create Your Ticket</h3>
+        <label>Title</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default TicketForm;

ae6b341e68ccded686f67390c0fb86e687ebfc73
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-13 13:24:14 -0300

    added css for form
    need to fix an error, can't test

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/TicketPage/[id]/page.jsx
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/TicketPage/[id]/page.jsx
@@ -1,5 +1,9 @@
+"in client";
+
+import TicketForm from "../../(components)/TicketForm";
+
 const TicketPage = ({ params }) => {
-  return <div>TicketPage {params.id}</div>;
+  return <TicketForm />;
 };
 
 export default TicketPage;

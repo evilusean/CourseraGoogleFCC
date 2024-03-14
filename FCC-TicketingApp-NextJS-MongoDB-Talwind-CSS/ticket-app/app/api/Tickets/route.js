07abf577903cdc87d414b1e4f1f140a636d3cef0
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-14 13:57:42 -0300

    Update route.js

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/api/Tickets/route.js
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/api/Tickets/route.js
@@ -5,10 +5,12 @@ export async function POST(req) {
   try {
     const body = await req.json();
     const ticketData = body.formData;
+
     await Ticket.create(ticketData);
 
     return NextResponse.json({ message: "Ticket Created!" }, { status: 201 });
-  } catch (error) {
-    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
 }

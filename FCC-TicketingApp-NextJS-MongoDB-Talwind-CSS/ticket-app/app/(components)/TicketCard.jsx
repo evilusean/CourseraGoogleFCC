2c2a4d6b429eac8c5a6195eb99dcecbc1dbbb0c4
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-14 21:00:06 -0300

    Update TicketCard.jsx
    added dynamic values to each ticket card

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketCard.jsx
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketCard.jsx
@@ -3,28 +3,26 @@ import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
-const TicketCard = () => {
+const TicketCard = ({ ticket }) => {
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
-        <PriorityDisplay />
+        <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
           <DeleteBlock />
         </div>
       </div>
       <h4>Ticket Title</h4>
       <hr className="h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap">
-        this is a ticket description! please do me!
-      </p>
+      <p className="whitespace-pre-wrap">{ticket.description}</p>
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">12/03/2024 13:42PM</p>
-          <ProgressDisplay />
+          <p className="text-xs my-1">{ticket.createdAT}</p>
+          <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="ml-auto flex items-end">
-          <StatusDisplay />
+          <StatusDisplay status={ticket.status} />
         </div>
       </div>
     </div>

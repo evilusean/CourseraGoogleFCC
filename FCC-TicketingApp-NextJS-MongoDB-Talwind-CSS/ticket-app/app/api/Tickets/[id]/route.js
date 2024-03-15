9db29c4996bcb6a0d73ac848edf3c3737de3a0eb
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-15 14:55:00 -0300

    Update route.js
    added GET request for Ticket _id

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/api/Tickets/[id]/route.js
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/api/Tickets/[id]/route.js
@@ -1,6 +1,12 @@
 import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
 
+export async function GET(req, { params }) {
+  const { id } = params;
+  const foundTicket = await Ticket.findOne({ _id: id });
+  return NextResponse.json({ foundTicket }, { status: 200 });
+}
+
 export async function DELETE(req, { params }) {
   try {
     const { id } = params;

97d2a39a1673ab72869b01514bf72dd778a40e16
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-15 15:02:21 -0300

    updated EDITMODE

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/TicketPage/[id]/page.jsx
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/TicketPage/[id]/page.jsx
@@ -17,9 +17,13 @@ const TicketPage = async ({ params }) => {
 
   if (EDITMODE) {
     updateTicketData = await getTicketById(params.id);
-    console.log(updateTicketData);
+    updateTicketData = updateTicketData.foundTicket;
+  } else {
+    updateTicketData = {
+      _id: "new",
+    };
   }
-  return <TicketForm />;
+  return <TicketForm ticket={updateTicketData} />;
 };
 
 export default TicketPage;
--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/api/Tickets/[id]/route.js
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/api/Tickets/[id]/route.js
@@ -2,9 +2,13 @@ import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
-  const { id } = params;
-  const foundTicket = await Ticket.findOne({ _id: id });
-  return NextResponse.json({ foundTicket }, { status: 200 });
+  try {
+    const { id } = params;
+    const foundTicket = await Ticket.findOne({ _id: id });
+    return NextResponse.json({ foundTicket }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
 }
 
 export async function DELETE(req, { params }) {

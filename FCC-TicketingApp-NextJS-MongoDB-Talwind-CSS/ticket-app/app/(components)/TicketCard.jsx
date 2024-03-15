63985bd3faf36d9528a158b3fbd9feb7d8def7ec
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-15 06:17:01 -0300

    Update TicketCard.jsx
    fixed bug, now appears correctly

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketCard.jsx
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/(components)/TicketCard.jsx
@@ -20,6 +20,8 @@ const TicketCard = ({ ticket }) => {
     return formattedDate;
   }
 
+  const createdDateTime = formatTimestamp(ticket.createdAt);
+
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
@@ -34,7 +36,7 @@ const TicketCard = ({ ticket }) => {
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">{formatTimestamp(ticket.createdAT)}</p>
+          <p className="text-xs my-1">{createdDateTime}</p>
           <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="ml-auto flex items-end">

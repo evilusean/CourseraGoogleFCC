c4c6266cda358d9a1bbd71f60c79fbb61459b613
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-15 10:44:05 -0300

    fixed bugs
    delete function should work with DB now

--- FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/api/Tickets/[id]/route.js
+++ FCC-TicketingApp-NextJS-MongoDB-Talwind-CSS/ticket-app/app/api/Tickets/[id]/route.js
@@ -1,8 +1,9 @@
-import Ticket from "@/app/models/Ticket";
+import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
 
 export async function DELETE(req, { params }) {
   try {
+    const { id } = params;
     await Ticket.findByIdAndDelete(id);
 
     return NextResponse.hson({ message: "Ticket Deleted" }, { status: 200 });

303619c16162f71f0c00b120de5d906ea133ffa5
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-19 15:11:43 -0300

    Update layout.tsx

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/layout.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
+      </body>
     </html>
   );
 }

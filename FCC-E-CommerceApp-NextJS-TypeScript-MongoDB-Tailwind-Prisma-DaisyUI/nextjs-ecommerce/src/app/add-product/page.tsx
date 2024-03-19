df1f1ff742c193bd20269e3988b3f6f8ad44531a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-19 14:38:20 -0300

    Update page.tsx
    added to our input form

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/add-product/page.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/add-product/page.tsx
@@ -2,6 +2,34 @@ export default function AddProductPage() {
   return (
     <div>
       <h1 className="text-lg mb-3 font-bold">Add Product</h1>
+      <form>
+        <input
+          required
+          name="name"
+          placeholder="Product Name"
+          className="mb-3 w-full input input-bordered"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Product Description"
+          className="mb-3 w-full textarea textarea-bordered"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="mb-3 w-full input input-bordered"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="mb-3 w-full input input-bordered"
+        />
+      </form>
     </div>
   );
 }

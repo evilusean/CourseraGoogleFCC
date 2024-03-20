4699bedd3df90ecf201d912065bf6d207b65f1e8
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-20 16:38:55 -0300

    added card component to main page

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/components/ProductCard.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/components/ProductCard.tsx
@@ -1,7 +1,19 @@
 import { Product } from "@prisma/client";
+import Link from "next/link";
 
 interface ProductCardProps {
   product: Product;
 }
 
-export default function ProductCard({ product }: ProductCardProps) {}
+export default function ProductCard({ product }: ProductCardProps) {
+  return (
+    <Link
+      href={"/products/" + product.id}
+      className="card w-full bg-base-100 hover:shadow-xl transition-shadow"
+    >
+      <div className="card-body">
+        <h2 className="card-title">{product.name}</h2>
+      </div>
+    </Link>
+  );
+}

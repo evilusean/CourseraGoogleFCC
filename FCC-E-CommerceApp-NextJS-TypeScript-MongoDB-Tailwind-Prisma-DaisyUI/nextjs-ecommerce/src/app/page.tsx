80d4402991f0935afbbfc1164e40d9b61ea8b433
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-21 16:16:28 -0300

    Update page.tsx
    added responsive tailwind and dynamic product title

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/page.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/page.tsx
@@ -10,14 +10,19 @@ export default async function Home() {
   return (
     <div>
       <div className="hero rounded-xl bg-base-200">
-        <Image
-          src={products[0].imageUrl}
-          alt={products[0].name}
-          width={400}
-          height={800}
-          className="w-full max-w-sm rounded-lg shadow-2xl"
-          priority
-        />
+        <div className="hero-content flex-col lg:flex-row">
+          <Image
+            src={products[0].imageUrl}
+            alt={products[0].name}
+            width={400}
+            height={800}
+            className="w-full max-w-sm rounded-lg shadow-2xl"
+            priority
+          />
+          <div>
+            <h1 className="text-5xl font-bold">{products[0].name}</h1>
+          </div>
+        </div>
       </div>
     </div>
   );

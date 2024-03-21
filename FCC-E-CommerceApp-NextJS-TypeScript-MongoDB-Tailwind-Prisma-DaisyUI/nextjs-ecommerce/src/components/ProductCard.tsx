85806c4411473f7c587cdcccb9fff2b968b70048
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-21 14:28:17 -0300

    Update ProductCard.tsx
    added images to productcard

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/components/ProductCard.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@prisma/client";
 import Link from "next/link";
 import PriceTag from "./PriceTag";
+import Image from "next/image";
 
 interface ProductCardProps {
   product: Product;
@@ -12,6 +13,15 @@ export default function ProductCard({ product }: ProductCardProps) {
       href={"/products/" + product.id}
       className="card w-full bg-base-100 hover:shadow-xl transition-shadow"
     >
+      <figure>
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={800}
+          height={400}
+          className="h-48 object-cover"
+        />
+      </figure>
       <div className="card-body">
         <h2 className="card-title">{product.name}</h2>
         <p>{product.description}</p>

cc74eafb0a44a6fd8156f123a286dd2548265114
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-21 19:56:55 -0300

    Update page.tsx
    added product page dynamic values

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/products/[id]/page.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/products/[id]/page.tsx
@@ -1,4 +1,7 @@
 import prisma from "@/app/lib/db/prisma";
+import PriceTag from "@/components/PriceTag";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
 interface ProductPageProps {
   params: {
@@ -12,4 +15,23 @@ export default async function ProductPage({
   const product = await prisma.product.findUnique({
     where: { id },
   });
+  if (!product) notFound();
+  return (
+    <div className="flex flex-col lg:flex-row ">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+
+      <div className="flex flex-col justify-between">
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4" />
+        <p className="py-6">{product.description}</p>
+      </div>
+    </div>
+  );
 }

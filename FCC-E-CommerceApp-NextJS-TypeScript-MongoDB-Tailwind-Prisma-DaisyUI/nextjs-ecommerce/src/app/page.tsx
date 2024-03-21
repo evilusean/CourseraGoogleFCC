ec7e165835bfd2696aff06f025287f8030b51158
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-21 16:27:37 -0300

    Update page.tsx
    added responsive product grid

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/page.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/page.tsx
@@ -32,6 +32,11 @@ export default async function Home() {
           </div>
         </div>
       </div>
+      <div className="my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        {products.slice(1).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 }

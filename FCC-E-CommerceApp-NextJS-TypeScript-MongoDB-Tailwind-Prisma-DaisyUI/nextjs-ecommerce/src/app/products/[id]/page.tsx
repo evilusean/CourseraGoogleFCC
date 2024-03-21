3a35762e9fbeb37de217cfdb37e18b92c874a30c
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-21 17:04:37 -0300

    Create page.tsx
    created unique id finder for product pages, also fixed routing folder setup

---
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/products/[id]/page.tsx
@@ -0,0 +1,15 @@
+import prisma from "@/app/lib/db/prisma";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+}

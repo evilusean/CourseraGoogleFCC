a3974cc9874e4cd0db8141c307877f6cc0fe8e2e
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-22 14:08:41 -0300

    Update AddToCartButton.tsx
    fixed typo

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/products/[id]/AddToCartButton.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/products/[id]/AddToCartButton.tsx
@@ -2,13 +2,12 @@ interface AddToCartButtonProps {
   productId: string;
 }
 
-export default function AddToCartButton({productId}: AddToCartButtonProps) {
- return (
+export default function AddToCartButton({ productId }: AddToCartButtonProps) {
+  return (
     <div className="flex items-center gap-2">
-        <button className="btn btn-primary" onClick={()}>
-            Add to Cart
-        </button>
+      <button className="btn btn-primary" onClick={() => {}}>
+        Add to Cart
+      </button>
     </div>
- )
-
+  );
 }

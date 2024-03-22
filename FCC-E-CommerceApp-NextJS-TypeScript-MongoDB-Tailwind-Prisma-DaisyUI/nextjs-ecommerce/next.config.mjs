b0c13ca3e3d6e4b5016367502ac6946c321a65a7
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-22 07:11:41 -0300

    Update next.config.mjs
    fixed display bug, wasn't loading from site, added in 6 more products

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/next.config.mjs
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "images.unsplash.com" }],
+    remotePatterns: [
+      { hostname: "images.unsplash.com" },
+      { hostname: "plus.unsplash.com" },
+    ],
   },
   experimental: {
     serverActions: true,

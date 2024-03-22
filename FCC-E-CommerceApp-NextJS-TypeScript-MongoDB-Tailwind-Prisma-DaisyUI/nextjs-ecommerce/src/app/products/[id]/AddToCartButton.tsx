652c5df286cb1495a2f115e77dad0e729266d387
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-22 14:08:08 -0300

    Create AddToCartButton.tsx
    created button component

---
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/products/[id]/AddToCartButton.tsx
@@ -0,0 +1,14 @@
+interface AddToCartButtonProps {
+  productId: string;
+}
+
+export default function AddToCartButton({productId}: AddToCartButtonProps) {
+ return (
+    <div className="flex items-center gap-2">
+        <button className="btn btn-primary" onClick={()}>
+            Add to Cart
+        </button>
+    </div>
+ )
+
+}

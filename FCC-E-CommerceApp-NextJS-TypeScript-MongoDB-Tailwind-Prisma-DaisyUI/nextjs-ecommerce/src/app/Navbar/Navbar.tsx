9c1c1c75decf0559ab60746c97920f6279d8137c
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-24 15:48:52 -0300

    Update Navbar.tsx
    added button to navbar

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/Navbar/Navbar.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/Navbar/Navbar.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../assets/logo.png";
+import { redirect } from "next/navigation";
 
-export default function Navbar() {
+async function searchProducts(formData: FormData) {
+  "use server";
+
+  const searchQuery = formData.get("searchQuery")?.toString();
+
+  if (searchQuery) {
+    redirect("/search?query=" + searchQuery);
+  }
+}
+
+export default async function Navbar() {
+  const cart = await getCart();
+
   return (
     <div className="bg-base-100">
       <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
@@ -13,7 +26,7 @@ export default function Navbar() {
           </Link>
         </div>
         <div className="flex-none gap-2">
-          <form>
+          <form action={searchProducts}>
             <div className="form-control">
               <input
                 name="searchQuery"
@@ -22,6 +35,7 @@ export default function Navbar() {
               />
             </div>
           </form>
+          <ShoppingCartButton cart={cart} />
         </div>
       </div>
     </div>

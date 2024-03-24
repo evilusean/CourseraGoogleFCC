6d308c0c21059d1d278875c5806fc416110e5005
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-24 14:14:11 -0300

    Update Navbar.tsx
    @ symbol wasn't working for some reason

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/Navbar/Navbar.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/assets/logo.png";
+import logo from "../assets/logo.png";
 
 export default function Navbar() {
   return (

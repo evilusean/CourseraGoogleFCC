2b1dd75589a74708253141996b84ed6ce6b33a13
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-24 18:18:09 -0300

    Update layout.tsx
    added footer to layout

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/layout.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
+        <Footer />
       </body>
     </html>
   );

f6ad25c33a40d96b69d69289a43e5ee90333923e
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-24 14:18:14 -0300

    Update Navbar.tsx
    added search input field to navbar

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/Navbar/Navbar.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/Navbar/Navbar.tsx
@@ -12,6 +12,17 @@ export default function Navbar() {
             Flowmazon
           </Link>
         </div>
+        <div className="flex-none gap-2">
+          <form>
+            <div className="form-control">
+              <input
+                name="searchQuery"
+                placeholder="Search"
+                className="input input-bordered w-full min-w-[100px]"
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );

2a46247a18b17c42490f54171577880c375a8efb
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-25 08:54:29 -0300

    Update CartEntry.tsx
    added 1-99 for quantity options

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/cart/CartEntry.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/cart/CartEntry.tsx
@@ -12,6 +12,14 @@ interface CartEntryProps {
 export default function CartEntry({
   cartItem: { product, quantity },
 }: CartEntryProps) {
+  const quantityOptions: JSX.Element[] = [];
+  for (let i = 1; i <= 99; i++) {
+    quantityOptions.push(
+      <option value={i} key={i}>
+        {i}
+      </option>,
+    );
+  }
   return (
     <div>
       <div className="flex flex-wrap items-center gap-3">
@@ -27,6 +35,10 @@ export default function CartEntry({
             {product.name}
           </Link>
           <div>Price: {formatPrice(product.price)}</div>
+          <div className="my-1 fkex items-center gap-2">
+            Quantity:
+            <select>{quantityOptions}</select>
+          </div>
           <div className="flex items-center gap-3">
             Total: {formatPrice(product.price * quantity)}
           </div>

9d27f61d6f0077de0af54239f59c1bf58d4a62e8
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-25 08:45:53 -0300

    Update page.tsx
    added cart images to cart

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/cart/page.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/cart/page.tsx
@@ -1,18 +1,19 @@
+import { getCart } from "../lib/db/cart";
+import CartEntry from "./CartEntry";
+
 export const metadata = {
-  title: 'Your Shopping Cart',
-  description: 'This is the shopping cart page.',
+  title: "Your Shopping Cart",
+  description: "This is the shopping cart page.",
 };
 
-
 export default async function CartPage() {
-
-    const cart = await getCart();
+  const cart = await getCart();
 
   return (
     <div>
       <h1 className="text-3xl font-bold mb-6">Shopping Cart</h1>
-      {cart?.items.map(cartItem => (
-        
+      {cart?.items.map((cartItem) => (
+        <CartEntry cartItem={cartItem} key={cartItem.id} />
       ))}
     </div>
   );

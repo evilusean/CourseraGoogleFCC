00493d2efe9f28e1f511aceda3477bff386112bf
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-25 08:43:36 -0300

    Update CartEntry.tsx
    added cartentry image

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/cart/CartEntry.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/cart/CartEntry.tsx
@@ -1,3 +1,27 @@
 "use client";
 
-export default function CartEntry() {}
+import Image from "next/image";
+import { CartItemithProduct } from "../lib/db/cart";
+
+interface CartEntryProps {
+  cartItem: CartItemithProduct;
+}
+
+export default function CartEntry({
+  cartItem: { product, quantity },
+}: CartEntryProps) {
+  return (
+    <div>
+      <div className="fkex flex-wrap items-center gap-3">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={200}
+          height={200}
+          className="rounded-lg"
+        />
+      </div>
+      <div className="divider" />
+    </div>
+  );
+}

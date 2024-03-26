c66fde8018c09c193275f2aa1049efd285f84d95
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-26 19:12:46 -0300

    Update next.config.mjs
    updated to allow user profile for content

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/next.config.mjs
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     remotePatterns: [
       { hostname: "images.unsplash.com" },
       { hostname: "plus.unsplash.com" },
+      { hostname: "lh3.googleusercontent.com" },
     ],
   },
   experimental: {

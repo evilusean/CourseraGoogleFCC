99ea11b4034cf23938a36175893f6c978a732a29
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-26 15:05:30 -0300

    .env schema
    added routehandler and .env environment variables schema using zod

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/api/auth/[...nextauth]/route.ts
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,20 @@
+import prisma from "@/app/lib/db/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions: NextAuthOptions = {};
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };

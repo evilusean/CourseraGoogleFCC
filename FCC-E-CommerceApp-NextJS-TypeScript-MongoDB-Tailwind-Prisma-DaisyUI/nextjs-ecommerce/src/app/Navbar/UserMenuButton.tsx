35360089c185a7e9e684c7f2b9430833a0eacf3b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-26 16:29:31 -0300

    Update UserMenuButton.tsx
    need to find svg image,

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/Navbar/UserMenuButton.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/Navbar/UserMenuButton.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import { Session } from "next-auth";
 
 interface UserMenuButtonProps {
   session: Session | null;
 }
 
-export default function UserMenuButton() {
-  return <div></div>;
+export default function UserMenuButton({session}: UserMenuButtonProps) {
+    const user = session?.user;
+
+  return (<div className="dropdown dropdown-end">
+    <label tabIndex={0} className="btn btn-ghost btn-circle">
+        {user ?
+        <Image /> : 
+        }
+    </label>
+  </div>);
 }

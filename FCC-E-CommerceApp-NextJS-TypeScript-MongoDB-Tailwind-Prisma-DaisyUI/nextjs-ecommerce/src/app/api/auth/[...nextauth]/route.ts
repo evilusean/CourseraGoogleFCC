5409a945f067a38afdbd9cfb6a70152b77a57d1a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-26 15:07:23 -0300

    Update route.ts
    fixed red squigglies with zod

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/api/auth/[...nextauth]/route.ts
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import prisma from "@/app/lib/db/prisma";
+import { env } from "@/app/lib/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
@@ -9,8 +10,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 };

343501bcfa06a5addc887a0427a3ce5aeae10203
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-27 17:35:51 -0300

    Update page.tsx
    added authentication session check to ensure user is logged in before adding a product

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/add-product/page.tsx
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/add-product/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import prisma from "../lib/db/prisma";
 import FormSubmitButton from "@/components/FormSubmitButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export const metadata = {
   title: "Add Product Page",
@@ -30,7 +32,12 @@ async function addProduct(formData: FormData) {
   redirect("/");
 }
 
-export default function AddProductPage() {
+export default async function AddProductPage() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/add-product");
+  }
+
   return (
     <div>
       <h1 className="text-lg mb-3 font-bold">Add Product</h1>

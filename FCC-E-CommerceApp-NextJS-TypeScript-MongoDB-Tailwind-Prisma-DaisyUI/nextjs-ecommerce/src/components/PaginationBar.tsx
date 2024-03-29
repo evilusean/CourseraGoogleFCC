aa2f10b2c033df9bf8bdf50194e794f72f595780
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-29 17:34:11 -0300

    Create PaginationBar.tsx
    .min returns smallest of values passed, .max opposite, currentPage +4 will be max 13=17, and on page 1 will show pages up until 10 or else it would be small
      if there is no page +4 then it will show total pages instead

---
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/components/PaginationBar.tsx
@@ -0,0 +1,12 @@
+interface PaginationBarProps {
+  currentPage: number;
+  totalPages: number;
+}
+
+export default function PaginationBar({
+  currentPage,
+  totalPages,
+}: PaginationBarProps) {
+  const maxPage = Math.min(totalPages, Math.max(currentPage + 4, 10));
+  const minPage = Math.max(1, Math.min(currentPage - 5, maxPage - 9));
+}

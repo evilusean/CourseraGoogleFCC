2489bdbe0233b457f1076009e06678bb90a8ce14
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-03-30 17:14:16 -0300

    Update actions.ts
    going back to original and adding items then going to update this, iadding a bunch of items, will change back after, already know this works

--- FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/cart/actions.ts
+++ FCC-E-CommerceApp-NextJS-TypeScript-MongoDB-Tailwind-Prisma-DaisyUI/nextjs-ecommerce/src/app/cart/actions.ts
@@ -11,27 +11,15 @@ export async function setProductQuantity(productId: string, quantity: number) {
 
   if (quantity === 0) {
     if (articleInCart) {
-      await prisma.cart.update({
-        where: { id: cart.id },
-        data: {
-          items: {
-            delete: { id: articleInCart.id },
-          },
-        },
+      await prisma.cartItem.delete({
+        where: { id: articleInCart.id },
       });
     }
   } else {
     if (articleInCart) {
-      await prisma.cart.update({
-        where: { id: cart.id },
-        data: {
-          items: {
-            update: {
-              where: { id: articleInCart.id },
-              data: { quantity },
-            },
-          },
-        },
+      await prisma.cartItem.update({
+        where: { id: articleInCart.id },
+        data: { quantity },
       });
     } else {
       await prisma.cartItem.create({

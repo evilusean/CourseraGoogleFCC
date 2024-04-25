09368abc4432c11361177221fc21ce4137f7b507
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-25 19:47:38 -0300

    Update index.js
    added typeDefs schema to apollo server

--- FCC- GraphQL-NodeJS - Apollo/Lesson 3/index.js
+++ FCC- GraphQL-NodeJS - Apollo/Lesson 3/index.js
@@ -1,9 +1,10 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
 
 // server setup
 const server = new ApolloServer({
-  // typeDefs
+  typeDefs,
   // resolvers
 });
 

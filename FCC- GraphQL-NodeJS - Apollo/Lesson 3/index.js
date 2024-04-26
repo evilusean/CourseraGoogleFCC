a82e220e190d46186a5bd15dc95122234f3d5927
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-26 04:24:23 -0300

    Update index.js

--- FCC- GraphQL-NodeJS - Apollo/Lesson 3/index.js
+++ FCC- GraphQL-NodeJS - Apollo/Lesson 3/index.js
@@ -1,11 +1,26 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
 
 // server setup
 const server = new ApolloServer({
   typeDefs,
-  // resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {

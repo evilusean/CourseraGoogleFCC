d5342e0e1ee48ee0e55b67513da891c855448965
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-28 01:32:51 -0300

    Update index.js
    added update game resolver

--- FCC- GraphQL-NodeJS - Apollo/Lesson 3/index.js
+++ FCC- GraphQL-NodeJS - Apollo/Lesson 3/index.js
@@ -57,6 +57,17 @@ const resolvers = {
 
       return db.games;
     },
+    updateGame(_, args) {
+      db.games = db.games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits };
+        }
+
+        return g;
+      });
+
+      return db.games.find((g) => g.id === args.id);
+    },
   },
 };
 

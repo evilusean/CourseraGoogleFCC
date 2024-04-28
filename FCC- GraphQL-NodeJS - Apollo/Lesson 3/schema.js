d5bd0b2b0255e325e8e72df733c7650fb57c407a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-28 01:28:00 -0300

    Update schema.js

--- FCC- GraphQL-NodeJS - Apollo/Lesson 3/schema.js
+++ FCC- GraphQL-NodeJS - Apollo/Lesson 3/schema.js
@@ -29,6 +29,7 @@ export const typeDefs = `#graphql
   type Mutation {
     addGame(game: AddGameInput!): Game
     deleteGame(id: ID!): [Game]
+    updateGame(id: ID!, edits: EditGameInput): Game
   }
   input AddGameInput {
     title: String!,

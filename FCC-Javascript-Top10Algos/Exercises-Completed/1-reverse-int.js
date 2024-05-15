333abd047430da3345a7bcccde66c45d17a509dd
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-15 00:40:43 -0300

    tried 2 functions for same result,
    next trying to built in function

--- FCC-Javascript-Top10Algos/Exercises-Completed/1-reverse-int.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/1-reverse-int.js
@@ -8,12 +8,38 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+/* Old way of doing it, with more moving parts. Below this function is one with new syntax for(let char of str) {}
 function reverse(str) {
+  let reversed = "";
 
+  for (let i = 0; i < str.length; i++) {
+    reversed = str[i] + reversed;
+  }
+  return reversed;
+}
+*/
 
+// new syntax for(let char of str) {} way of doing it :
+// function reverse(str) {
+//     let reversed = "";
+
+//     for (let char of str) {
+//       reversed = char + reversed;
+//     }
+//     return reversed;
+//   }
+
+// Built in Function way of doing it : Array.prototype.reverse() # can only be used on arrays, convert a string into an array and do the reverse.
+
+function reverse(str) {
+  let reversed = "";
+
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
 }
 
-function reverseInt(n) {
+console.log(reverse("hello"));
 
-  
-}
+function reverseInt(n) {}

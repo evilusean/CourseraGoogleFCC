5cc40337afcada80dd63b45efa40fe2cf6ecdfa0
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-15 23:09:18 -0300

    Update 2-palindrome.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/2-palindrome.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/2-palindrome.js
@@ -31,3 +31,42 @@ console.log(palindrome("Phat"));
 JS built in Method 'Every' =
  https://www.w3schools.com/jsref/jsref_every.asp#:~:text=The%20every()%20method%20returns,the%20function%20for%20empty%20elements.
 */
+/* 2 Pointers Technique:
+function isPairSum(A, N, X)
+{
+        for (var i = 0; i < N-1; i++)
+        {
+            for (var j = i+1; j < N; j++)
+            {
+                // as equal i and j means same element
+                if (i == j)
+                    continue;
+
+                // pair exists
+                if (A[i] + A[j] == X)
+                    return 1;
+
+                // as the array is sorted
+                if (A[i] + A[j] > X)
+                    break;
+            }
+        }
+
+        // No pair found with given sum.
+        return 0;
+}
+
+
+    
+        var arr=[ 2, 3, 5, 8, 9, 10, 11 ];
+        
+        // value to search
+        var val = 17;
+    
+        // size of the array
+        var arrSize = 7;
+    
+        // Function call
+        document.write(isPairSum(arr, arrSize, val));
+
+*/

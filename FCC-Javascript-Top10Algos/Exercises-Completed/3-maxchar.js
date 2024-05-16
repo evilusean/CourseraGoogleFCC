95946dd375e90fc8c104d64941e7e6faeffe52fc
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-16 08:08:13 -0300

    Update 3-maxchar.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/3-maxchar.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/3-maxchar.js
@@ -5,7 +5,7 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-// Will go through each character in the string and map it to the character Map
+// METHOD 1: WILL NOT SORT THE HIGHEST USE CHAR Will go through each character in the string and map it to the character Map
 // function maxChar(str) {
 //   const charMap = {}; //create an empty object to hold all the characters we loop through
 //   for (let char of str) {
@@ -18,7 +18,8 @@
 //   return charMap;
 // }
 
-//Object.entries(charMap) METHOD: Will sort through the object we created and turn it into an array with key:value pairs, so we can find the maxChar
+//METHOD 2 :Object.entries(charMap) Will sort through the object we created and turn it into an array with key:value pairs, so we can find the maxChar
+/* CODE WORKS, WE ARE DOING A NEW WAY TO OPTIMIZE
 function maxChar(str) {
   const charMap = {}; //create an empty object to hold all the characters we loop through
   let max = 0; // to find the maxCharacter(character used the most) we need to have a variable that keeps track of it
@@ -32,11 +33,36 @@ function maxChar(str) {
     }
   }
   for (const [key, value] of Object.entries(charMap)) {
+    //destructure [key, value] from the object, and converts into an array
     //will convert each key:value letter:count in the charMap object into an array, this will loop through each key:value pair in the array
     if (value > max) {
       //if the value of the current key is greater than the current max,
       max = value; //set new max is current value count
       maxChar = key; //set new maxChar as the current key
+    }
+  }
+  return maxChar;
+}
+*/
+
+//METHOD 3: OPTIMIZED for(let key in charMap): Nicer, cleaner code
+//When you want to loop through an array of strings use 'of' / when you
+function maxChar(str) {
+  const charMap = {}; //create an empty object to hold all the characters we loop through
+  let max = 0; // to find the maxCharacter(character used the most) we need to have a variable that keeps track of it
+  let maxChar = ""; // same as above but for the key, instead of the count
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char] = charMap[char] + 1;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  for (let key in charMap) {
+    if (charMap[key] > max) {
+      max = charMap[key];
+      maxChar = key;
     }
   }
   return maxChar;

c0f1aa6f35a8c8b620a37f8dcd0ae78a5a68573c
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-15 22:54:49 -0300

    Update 2-palindrome.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/2-palindrome.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/2-palindrome.js
@@ -8,4 +8,10 @@
 //   palindrome("madam") === true
 //   palindrome("codingmoney") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  const reveresed = str.split("").reverse().join(""); //first we need to find the reverse of our input string, first .split turns it into an array,
+  // .reverse inverts the array, and .join turns it back into a string
+  if (str === reversed) {
+    return true; // if input str, is equal to reversed(above variable) return true(it is a palindrome)
+  }
+}

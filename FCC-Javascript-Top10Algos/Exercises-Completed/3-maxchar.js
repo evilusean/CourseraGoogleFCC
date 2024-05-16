0c89d02fa9eb5a8bb281536541a155242bf9e4e4
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-16 02:54:34 -0300

    Update 3-maxchar.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/3-maxchar.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/3-maxchar.js
@@ -5,13 +5,38 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// Will go through each character in the string and map it to the character Map
+// function maxChar(str) {
+//   const charMap = {}; //create an empty object to hold all the characters we loop through
+//   for (let char of str) {
+//     if (charMap[char]) {
+//       charMap[char] = charMap[char] + 1;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   }
+//   return charMap;
+// }
+
+//Object.entries(charMap) METHOD: Will sort through the object we created and turn it into an array with key:value pairs, so we can find the maxChar
 function maxChar(str) {
   const charMap = {}; //create an empty object to hold all the characters we loop through
+  let max = 0; // to find the maxCharacter(character used the most) we need to have a variable that keeps track of it
+  let maxChar = ""; // same as above but for the key, instead of the count
+
   for (let char of str) {
     if (charMap[char]) {
       charMap[char] = charMap[char] + 1;
     } else {
       charMap[char] = 1;
+    }
+  }
+  for (const [key, value] of Object.entries(charMap)) {
+    //will convert each key:value letter:count in the charMap object into an array
+    if (value > max) {
+      //if the value of the current key is greater than the current max,
+      max = value; //set new max is current value count
+      maxChar = key; //set new maxChar as the current key
     }
   }
   return charMap;

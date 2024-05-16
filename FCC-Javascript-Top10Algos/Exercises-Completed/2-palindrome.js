1d0426130b61f56990e5d13f058bc81a0b1854fd
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-15 23:05:36 -0300

    Update 2-palindrome.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/2-palindrome.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/2-palindrome.js
@@ -2,19 +2,32 @@
 // Given a string, return true if the string is a palindrome
 // or false if it is not.  Palindromes are strings that
 // form the same word if it is reversed.
-
 // --- Examples:
 //   palindrome("kayak") === true
 //   palindrome("madam") === true
 //   palindrome("codingmoney") === false
 
+// function palindrome(str) {
+//   const reversed = str.split("").reverse().join(""); //first we need to find the reverse of our input string, first .split turns it into an array,
+//   // .reverse inverts the array, and .join turns it back into a string
+//   if (str === reversed) {
+//     return true; // if input str, is equal to reversed(above variable) return true(it is a palindrome)
+//   }
+//   return false; //otherwise return false
+// }
+
+//Turn all of the above code into a one liner
 function palindrome(str) {
   const reversed = str.split("").reverse().join(""); //first we need to find the reverse of our input string, first .split turns it into an array,
   // .reverse inverts the array, and .join turns it back into a string
-  if (str === reversed) {
-    return true; // if input str, is equal to reversed(above variable) return true(it is a palindrome)
-  }
-  return false; //otherwise return false
+  return str === reversed;
 }
 
 console.log(palindrome("refer"));
+console.log(palindrome("Phat"));
+
+/* FUTURE SEAN PROBLEMS: This problem can be solved in 2 other MEthods :
+2 Pointers Technique = https://www.geeksforgeeks.org/two-pointers-technique/ 
+JS built in Method 'Every' =
+ https://www.w3schools.com/jsref/jsref_every.asp#:~:text=The%20every()%20method%20returns,the%20function%20for%20empty%20elements.
+*/

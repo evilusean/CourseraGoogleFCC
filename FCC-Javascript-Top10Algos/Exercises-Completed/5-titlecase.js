5f61db87c9fa02f4c637cb34c23433d4cf7a1587
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-17 08:05:11 -0300

    Update 5-titlecase.js
    just tested, works, doesn't .join yet

--- FCC-Javascript-Top10Algos/Exercises-Completed/5-titlecase.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/5-titlecase.js
@@ -7,6 +7,14 @@
 //   capitalize('what is titlecase?') --> 'What Is Titlecase?'
 //   capitalize('titles of books, movies, songs, plays and other works') --> 'Titles Of Books, Movies, Songs, Plays And Other Works'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const words = str.split(" "); // wil split the strong at every " " space and save it as an array
+  const result = []; //empty array to store the new string, capitalize each word and push it into this array
+  for (let word of words) {
+    // for each word in words variable that was split at " " space,
+    result.push(word[0].toUpperCase() + word.slice(1)); //will capitalize first letter, and then add the rest of the word after .slice will take[1] index
+  }
+  return result; //return the pushed capitalized letter + words
+}
 
-console.log(capitalize("this is mukhtar from coding money"));
+console.log(capitalize("this is Sean from coding money"));

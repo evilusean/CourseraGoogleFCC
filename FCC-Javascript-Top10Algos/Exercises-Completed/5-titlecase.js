7aa62789f57a98203ea607f0aab7691c8bcb8c84
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-17 08:37:59 -0300

    Update 5-titlecase.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/5-titlecase.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/5-titlecase.js
@@ -7,6 +7,7 @@
 //   capitalize('what is titlecase?') --> 'What Is Titlecase?'
 //   capitalize('titles of books, movies, songs, plays and other works') --> 'Titles Of Books, Movies, Songs, Plays And Other Works'
 
+/*METHOD 1: Create an array, index [0] .toUpperCase, + .slice[1], .join(" ")
 function capitalize(str) {
   const words = str.split(" "); // wil split the strong at every " " space and save it as an array
   const result = []; //empty array to store the new string, capitalize each word and push it into this array
@@ -14,7 +15,13 @@ function capitalize(str) {
     // for each word in words variable that was split at " " space,
     result.push(word[0].toUpperCase() + word.slice(1)); //will capitalize first letter, and then add the rest of the word after .slice will take[1] index
   }
-  return result; //return the pushed capitalized letter + words
+  return result.join(" "); //return the pushed capitalized letter + words
+}
+*/
+//METHOD 2: Map
+function capitalize(str) {
+  const words = str.split(" "); // wil split the strong at every " " space and save it as an array
+  return words.map((word) => word[0].toUpperCase() + word.slice(1)).join(" ");
 }
 
 console.log(capitalize("this is Sean from coding money"));

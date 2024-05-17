781a8c9db45d8be000dacfc8735e154a7e40ba80
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-17 02:49:18 -0300

    Update 4-array-chunking.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/4-array-chunking.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/4-array-chunking.js
@@ -8,14 +8,16 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+//METHOD 1: WORKS
 function chunk(array, size) {
-  const result = [];
-  let index = 0;
+  const result = []; //ARRAY TO STORE PUSHED VALUES ON
+  let index = 0; //DEFAULT STARTS AT 0 '0 BASED INDEX'
   while (index < array.length) {
-    result.push(array.slice(index, index + size));
-    index += size;
+    //WHILE INDEX IS LESS THAN ARRAY.LENGTH,
+    result.push(array.slice(index, index + size)); //PUSH NEW VALUES, array.slice (INDEX VALUE, INDEX VALUE + SIZE) # WILL RETURN NEW SLICE FOR ARRAY
+    index += size; //ADDS NEXT SIZE TO INDEX, SO WE CAN PUSH ON THE NEXT SLICE TO THE ARRAY
   }
-  return result;
+  return result; //RETURNS THE NEW ARRAY
 }
 
 console.log(chunk([1, 2, 3, 4, 5], 4));

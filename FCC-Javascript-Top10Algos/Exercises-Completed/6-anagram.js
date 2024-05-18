327060d99974d77764b74abb32c3b74574a1f548
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-17 22:07:02 -0300

    Update 6-anagram.js
    fixed regex, forgot to declare as variable

--- FCC-Javascript-Top10Algos/Exercises-Completed/6-anagram.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/6-anagram.js
@@ -11,7 +11,7 @@
 // METHOD 1: Build a character map for stringA, then build character map for stringB, then compare the maps to see if they are equal
 function anagrams(stringA, stringB) {
   const charmapA = {}; //start by creating an empty object for storing each character:count key:value pair
-  stringA.toLowerCase().replace(/[\W]/g, ""); //to remove all capital letters, and to remove all punctuation use a regex
+  stringA = stringA.toLowerCase().replace(/[\W]/g, ""); //to remove all capital letters, and to remove all punctuation use a regex
   // REGEX : '/[\W]/g CHARACTER SET: BIG 'W' WILL MATCH ALL CHARACTERS THAT ARE NOT A WORD
   return stringA;
 }

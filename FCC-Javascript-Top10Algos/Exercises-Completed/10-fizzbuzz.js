72f6555fe97ccbdad90102acae921d2cbd7fd07f
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-19 13:21:12 -0300

    Update 10-fizzbuzz.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/10-fizzbuzz.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/10-fizzbuzz.js
@@ -12,6 +12,18 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
-
-fizzBuzz(5);
+//% = modulo operator
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzBuzz(15);

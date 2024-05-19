0f5693f8cbf40b7cca9735cf40926206e638b1f9
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-19 12:15:55 -0300

    Update 7-countVowels.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/7-countVowels.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/7-countVowels.js
@@ -8,6 +8,21 @@
 //   vowels('Coding Money') --> 4
 //   vowels('why?') --> 0
 
-function vowels(str) {}
+/* METHOD 1: Regex : Built in JS function called 'match', returns null if there are no matches, will return an array with all the matches 
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi); //g flag = won't stop at first match, i flag = case insensitivty, can be caps or lower
+  return matches ? matches.length : 0; //if there are matches, return how many there are(.length) of array - will give us a number : or zero
+}
+*/
 
+//METHOD 2: create an array with all vowels inside, check string against the array, if there is a match increment counter, return the counter
+function vowels(str) {
+  const vowelCheck = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (let char of str.toLowerCase()) {
+    if (vowelCheck.includes(char)) count++;
+    //arrays have a built in helper function called 'includes', if it is a vowel, else it will skip, then add +1 count increment
+  }
+  return count;
+}
 console.log(vowels("Coding Money"));

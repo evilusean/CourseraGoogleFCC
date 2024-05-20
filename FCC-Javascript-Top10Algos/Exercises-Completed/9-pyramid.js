0c4f714d3cb05e45fe156cebd2822552a9894021
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-20 11:53:07 -0300

    Update 9-pyramid.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/9-pyramid.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/9-pyramid.js
@@ -14,6 +14,19 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const mid = Math.floor((2 * n - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let line = "";
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (col >= mid - row && col <= mid + row) {
+        line += "#";
+      } else {
+        line += " ";
+      }
+    }
+    console.log(line);
+  }
+}
 
 pyramid(3);

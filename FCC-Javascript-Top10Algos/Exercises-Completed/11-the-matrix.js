14999a860826653e2aecad0758aa74b454b9c0ab
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-05-21 10:35:34 -0300

    Update 11-the-matrix.js

--- FCC-Javascript-Top10Algos/Exercises-Completed/11-the-matrix.js
+++ FCC-Javascript-Top10Algos/Exercises-Completed/11-the-matrix.js
@@ -17,9 +17,41 @@
 
 function matrix(n) {
   const result = [];
+  let counter = 1,
+    startRow = 0,
+    endRow = n - 1,
+    startCol = 0,
+    endCol = n - 1;
   for (let i = 0; i < n; i++) {
     result.push([]);
   }
+  while (startRow <= endRow && startCol <= endCol) {
+    //Top
+    for (let i = startCol; i <= endCol; i++) {
+      result[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    //Right
+    for (let i = startRow; i <= endRow; i++) {
+      result[i][endCol] = counter;
+      counter++;
+    }
+    endCol--;
+    //Bottom
+    for (let i = endCol; i >= startCol; i--) {
+      result[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+    //Left
+    for (let i = endRow; i >= startRow; i--) {
+      result[i][startCol] = counter;
+      counter++;
+    }
+    startCol++;
+  }
+
   return result;
 }
 

bfb687b764590ce9ac80b77ce5acf1b6b08a9354
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-25 16:03:09 -0300

    Update index.js

--- FCC-ThreeJS-5Projects/1Basics/index.js
+++ FCC-ThreeJS-5Projects/1Basics/index.js
@@ -1,1 +1,26 @@
-console.log("Sean Teams wuz here");
+import * as THREE from "three";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true})
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+
+
+
+
+
+
+
+

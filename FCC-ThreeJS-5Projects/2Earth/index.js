1448ae9e3766d1ff63c606d9f4d067642291ed68
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-27 16:07:18 -0300

    Update index.js

--- FCC-ThreeJS-5Projects/2Earth/index.js
+++ FCC-ThreeJS-5Projects/2Earth/index.js
@@ -35,39 +35,39 @@ const earthMesh = new THREE.Mesh(geometry, material);
 earthGroup.add(earthMesh);
 
 const lightsMat = new THREE.MeshBasicMaterial({
-  map: loader.load("./textures/03_earthlights1k.jpg"),
-  blending: THREE.AdditiveBlending,
+  map: loader.load("./textures/03_earthlights1k.jpg"), // this is for actual city lights, aesthetic AF
+  blending: THREE.AdditiveBlending, //will allow you to see the original earth underneath it and blend it with the lights - very cool - lights will be on in day 
 });
-const lightsMesh = new THREE.Mesh(geometry, lightsMat);
-earthGroup.add(lightsMesh);
+const lightsMesh = new THREE.Mesh(geometry, lightsMat); //lights up the dark side of earth, reusing iso geo
+earthGroup.add(lightsMesh); //add to the earth group so you can change the values with the group
 
 const cloudsMat = new THREE.MeshStandardMaterial({
-  map: loader.load("./textures/04_earthcloudmap.jpg"),
-  transparent: true,
-  opacity: 0.8,
-  blending: THREE.AdditiveBlending,
-  alphaMap: loader.load('./textures/05_earthcloudmaptrans.jpg'),
+  map: loader.load("./textures/04_earthcloudmap.jpg"), //clouds asset, use loader to load
+  transparent: true, //makes the clouds transparent
+  opacity: 0.8, //makes semi see through clouds(mesh texture)
+  blending: THREE.AdditiveBlending, //blends clouds with earth topo and city lights
+  alphaMap: loader.load('./textures/05_earthcloudmaptrans.jpg'), //shows clouds 
   // alphaTest: 0.3,
 });
-const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
-cloudsMesh.scale.setScalar(1.003);
-earthGroup.add(cloudsMesh);
+const cloudsMesh = new THREE.Mesh(geometry, cloudsMat); //creates mesh from the texture and iso geo
+cloudsMesh.scale.setScalar(1.003); //makes this mesh slightly larger so it sits on top of the city lights and the topography
+earthGroup.add(cloudsMesh); //adds it to the earthgroup
 
 const fresnelMat = getFresnelMat();
 const glowMesh = new THREE.Mesh(geometry, fresnelMat);
 glowMesh.scale.setScalar(1.01);
 earthGroup.add(glowMesh);
 
-const stars = getStarfield({numStars: 2000});
+const stars = getStarfield({numStars: 2000}); //allows you to set the amount of stars using 'getStarfield.js'
 scene.add(stars);
 
-const sunLight = new THREE.DirectionalLight(0xffffff, 2.0);
-sunLight.position.set(-2, 0.5, 1.5);
-scene.add(sunLight);
+const sunLight = new THREE.DirectionalLight(0xffffff, 2.0); //adds white sunlight, 
+sunLight.position.set(-2, 0.5, 1.5); // sets the lights position
+scene.add(sunLight); //add the sunlight to the scene
 
 function animate() {
   requestAnimationFrame(animate);
-
+  //mesh.scale.setScalar(Math.cos(t * 0.001) + 1.0) //Will cause the orb to shrink and expand over and over again 2DaMoon
   earthMesh.rotation.y += 0.002; //Starts the earth rotation
   lightsMesh.rotation.y += 0.002;
   cloudsMesh.rotation.y += 0.0023;

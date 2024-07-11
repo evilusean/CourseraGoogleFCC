1f2468320c8dc15496eed083be63b5adc7ed3677
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-11 16:50:55 -0300

    Update index.js

---
+++ FCC -Typescript-PizzaApp/PizzaApp/index.js
@@ -0,0 +1,13 @@
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+
+const cashInRegister = 100
+const orderQeue = []
+
+/**
+ Challenge: Add a utility function "addNewPizza" that takes a pizza object and adds it to the menu
+ */

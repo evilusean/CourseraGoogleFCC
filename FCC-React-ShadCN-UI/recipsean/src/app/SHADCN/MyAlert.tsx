c7028a1e9c06b7b3bcdacb44e5d159d8600f3fe4
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-18 20:12:11 -0300

    added alert message

---
+++ FCC-React-ShadCN-UI/recipsean/src/app/SHADCN/MyAlert.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { AlertCircle } from "lucide-react"
+ 
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+
+function MyAlert() {
+  return (
+    <div className="w-[400px] p-4">
+            <Alert variant="destructive"> {/* TO CHANGE VARIANT, INTELLISENSE OR SWITCH To variant="default ON THIS LINE*/}
+    {/* TO CHANGE THE ICON, GO TO 'https://lucide.dev/icons/' and select a new one REPLACE 'ALERTCIRCLE' BELOW TO CHANGE ICON, 
+    NOTICE THE IMPORTS AT TOP '"from lucide-react"', that is where you can get new icons, and change them, simply change/add import  */ }
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle>Error</AlertTitle>
+      <AlertDescription>
+        TO CHANGE THE ICON, GO TO 'https://lucide.dev/icons/' AND IMPORT A NEW ONE, REPLACE THE 'AlertCircle' Above here
+      </AlertDescription>
+    </Alert>
+    </div>
+  )
+}
+
+export default MyAlert
--- FCC-React-ShadCN-UI/recipsean/src/app/page.tsx
+++ FCC-React-ShadCN-UI/recipsean/src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MyAccordion from './SHADCN/MyAccordion'
+import MyAlert from './SHADCN/MyAlert'
 
 function page() {
   return (
 
     <div>
       <MyAccordion />
+      <MyAlert />
     </div>
   )
 }

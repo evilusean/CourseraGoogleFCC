088ddc4682dc55009aae9a2e58c452407f741dee
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-18 18:38:54 -0300

    Update MyAccordion.tsx
    made it so it can create 2

--- FCC-React-ShadCN-UI/recipsean/src/app/SHADCN/MyAccordion.tsx
+++ FCC-React-ShadCN-UI/recipsean/src/app/SHADCN/MyAccordion.tsx
@@ -8,12 +8,18 @@ import {
 
 function MyAccordion() {
   return (
-<div className='w-[200px]'>
+<div className='w-[400px] p-4'>
 <Accordion type="single" collapsible>
   <AccordionItem value="item-1">
-    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionTrigger>Seans A Team</AccordionTrigger>
     <AccordionContent>
-      Yes. It adheres to the WAI-ARIA design pattern.
+      Testing the Dropdown component, my first ShadCN component, This is very cool, 
+    </AccordionContent>
+  </AccordionItem>
+  <AccordionItem value="item-2">
+    <AccordionTrigger>Seans B Team</AccordionTrigger>
+    <AccordionContent>
+      Sean Team 6(RIP) - CARTHAGO DELENDA EST - Ceterum (autem) censeo Carthaginem esse delendam ("Furthermore, I consider Carthage to need to be destroyed") - Based Cato
     </AccordionContent>
   </AccordionItem>
 </Accordion>

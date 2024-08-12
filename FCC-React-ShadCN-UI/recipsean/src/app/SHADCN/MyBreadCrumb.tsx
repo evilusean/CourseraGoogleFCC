ab2ee1eaf2b58ac2dae5c5ab2c40da830ec4f802
Sean Teams <shawntemesy@hotmail.com>
2024-08-12 15:31:34 -0300

    npx shadcn-ui@latest add breadcrumb

---
+++ FCC-React-ShadCN-UI/recipsean/src/app/SHADCN/MyBreadCrumb.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+  } from "@/components/ui/breadcrumb"
+
+function MyBreadCrumb() {
+  return (
+    <Breadcrumb>
+  <BreadcrumbList>
+    <BreadcrumbItem>
+      <BreadcrumbLink href="/">Home</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator />
+    <BreadcrumbItem>
+      <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator />
+    <BreadcrumbItem>
+      <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+    </BreadcrumbItem>
+  </BreadcrumbList>
+</Breadcrumb>
+  )
+}
+
+export default MyBreadCrumb
--- FCC-React-ShadCN-UI/recipsean/src/app/page.tsx
+++ FCC-React-ShadCN-UI/recipsean/src/app/page.tsx
@@ -5,19 +5,21 @@ import MyAlertDialog from './SHADCN/MyAlertDialog'
 import MyAspectRatio from './SHADCN/MyAspectRatio'
 import MyAvatar from './SHADCN/MyAvatar'
 import MyBadge from './SHADCN/MyBadge'
+import MyBreadCrumb from './SHADCN/MyBreadCrumb'
 
 function page() {
   return (
 
     <div>
       <MyAccordion />
-      <MyAlert />
+      <MyAlert /> 
       <MyAlertDialog text="Sean-AlertDialog" title="Sean-Title" content="Becomes Sean, Reuseable with props-Content" />
       <MyAlertDialog text="UnSean-Use Components in Typescript props" title="Sean-Title" content={ <MyAlert/> } />
       <MyAlertDialog text="UnSean-Use Components in Typescript props" title="Sean-Title" content={ <MyAccordion /> } />
       <MyAspectRatio />
       <MyAvatar />
       <MyBadge title="Badge+Props" variant="destructive" />
+      <MyBreadCrumb />
     </div>
   )
 }

707536d92af0b2f8945e4798179f2ce7f5576969
Sean Teams <shawntemesy@hotmail.com>
2024-10-28 06:09:46 -0300

    reconnect
    added reconnect function and 'depends_on'

--- FCC-DevOps-Docker-Node-Express-Redis-Mongo/Docker/index.js
+++ FCC-DevOps-Docker-Node-Express-Redis-Mongo/Docker/index.js
@@ -5,20 +5,25 @@ const { MONGO_IP, MONGO_PORT, MONGO_USER, MONGO_PASSWORD } = require("./config/c
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+
 /* Old hardcoded method:
 mongoose.connect("mongodb://seanjeev:mypassword@mongo:27017/?authSource=admin").then(() => {
 New method with dynamic variables that reference the 'config.js'
 */
-const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.log(err));
-
-
+const connectWithRetry = () => {
+    mongoose.connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => {
+        console.log(err);
+        setTimeout(connectWithRetry, 5000);
+    });
+}
 
 app.get("/", (req, res) => {
     res.send("<h1>Test success!!</h2>");

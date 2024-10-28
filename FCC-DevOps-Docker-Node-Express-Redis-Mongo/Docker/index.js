071a56208972fb50b39b537523a5aaf841f7f708
Sean Teams <shawntemesy@hotmail.com>
2024-10-28 05:31:21 -0300

    Update index.js

--- FCC-DevOps-Docker-Node-Express-Redis-Mongo/Docker/index.js
+++ FCC-DevOps-Docker-Node-Express-Redis-Mongo/Docker/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { MONGO_IP, MONGO_PORT, MONGO_USER, MONGO_PASSWORD } = require("./config");
+
 
 const app = express();
 
+/* Old hardcoded method:
 mongoose.connect("mongodb://seanjeev:mypassword@mongo:27017/?authSource=admin").then(() => {
+New method with dynamic variables that reference the 'config.js'
+*/
+mongoose.connect("mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin").then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
     console.log(err);

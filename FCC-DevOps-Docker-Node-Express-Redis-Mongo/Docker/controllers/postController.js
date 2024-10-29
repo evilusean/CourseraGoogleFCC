c6670ac1125fbedfd616022ee9db915570e6b8ea
Sean Teams <shawntemesy@hotmail.com>
2024-10-29 08:10:06 -0300

    Create postController.js

---
+++ FCC-DevOps-Docker-Node-Express-Redis-Mongo/Docker/controllers/postController.js
@@ -0,0 +1,34 @@
+const Post = require("../models/postModel")
+
+exports.getAllPosts = async (req, res, next) => {
+try {
+    const posts = await Post.find();
+    res.status(200).json({
+        status: "success",
+        results: posts.length,
+        data: {
+            posts,
+        }
+    });
+} catch (e) {
+    res.status(400).json({
+        status: "fail",
+    });
+}
+}
+
+exports.getOnePost = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json({
+            status: "success",
+            data: {
+                post,
+            }
+        });
+    } catch (e) {
+        res.status(400).json({
+            status: "fail",
+        });
+    }
+}

94642bc4384ae49165dc8aa530905c65585ad548
Sean Teams <shawntemesy@hotmail.com>
2024-10-31 10:00:56 -0300

    npm install bcryptjs
    adding a hashpassword

--- FCC-DevOps-Docker-Node-Express-Redis-Mongo/Docker/controllers/authController.js
+++ FCC-DevOps-Docker-Node-Express-Redis-Mongo/Docker/controllers/authController.js
@@ -1,8 +1,15 @@
 const User = require("../models/userModel")
+const bcrypt = require("bcryptjs")
+
 
 exports.signUp = async (req, res) => {
+    const {username, password} = req.body
+    const hashpassword = await bcrypt.hash(password, 12)
     try  {
-        const newUser = await User.create(req.body)
+        const newUser = await User.create({
+            username,
+            password: hashpassword
+    });
         res.status(201).json({
             status: "success",
             data: {

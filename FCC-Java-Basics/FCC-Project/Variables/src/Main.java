fc2f3a694fec211a1172908dcbe760199153245b
Sean Teams <shawntemesy@hotmail.com>
2024-11-24 09:02:14 -0400

    Update Main.java

--- FCC-Java-Basics/FCC-Project/Variables/src/Main.java
+++ FCC-Java-Basics/FCC-Project/Variables/src/Main.java
@@ -12,6 +12,12 @@ public class Main {
         // notice the 'L' at the end, you will default be considred an integer even with the 'long' prefix before the name 'myNumber' - you need to add 'l'
         short maxShort = Short.MAX_VALUE;
         short minShort = Short.MIN_VALUE;
+        float maxFloat = Float.MAX_VALUE;
+        float minFloat = Float.MIN_VALUE;
+        float myFloat = 3.14F;
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+        double myDouble = 12.12;
 
         System.out.println("Hello " + name + ". Your age is " + age);
         System.out.println("The Maximum Value is " + max);
@@ -22,7 +28,10 @@ public class Main {
         System.out.println("The maximum long value is " + maxLongValue);
         System.out.println("The maximum short value is " + maxShort);
         System.out.println("The minimum short value is " + minShort);
-
+        System.out.println("The maximum float value is " + maxFloat);
+        System.out.println("The minimum float value is " + minFloat);
+        System.out.println("The maximum double value is " + maxDouble);
+        System.out.println("The minimum double value is " + minDouble);
 
     }
 }

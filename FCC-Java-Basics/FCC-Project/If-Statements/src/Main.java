394bb5c05014ea69bc27b994a4b543f27c8d148e
Sean Teams <shawntemesy@hotmail.com>
2024-11-25 03:50:41 -0400

    Voooooooootttteeeeeeeee
    can't help myself.
    Step 1) Escape.

--- FCC-Java-Basics/FCC-Project/If-Statements/src/Main.java
+++ FCC-Java-Basics/FCC-Project/If-Statements/src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int age = 17;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if (age >= 18) {
+            System.out.println("You can VOOOOOOOOOOTTTEEEEEEEE");
+            System.out.println("Do you want the PP or the turdeau");
+            System.out.println("Do you want the red puppet or the blue one");
+        } else {
+            System.out.println("If voting mattered would they let you do it?");
         }
     }
 }

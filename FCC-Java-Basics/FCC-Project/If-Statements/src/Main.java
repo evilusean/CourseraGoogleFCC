974c5ebe9a66151fbd648bf4f6f8c952cea5a326
Sean Teams <shawntemesy@hotmail.com>
2024-11-25 04:37:10 -0400

    Update Main.java

--- FCC-Java-Basics/FCC-Project/If-Statements/src/Main.java
+++ FCC-Java-Basics/FCC-Project/If-Statements/src/Main.java
@@ -3,13 +3,22 @@
 public class Main {
     public static void main(String[] args) {
         int age = 17;
-
+        //If Else statement
         if (age >= 18) {
             System.out.println("You can VOOOOOOOOOOTTTEEEEEEEE");
             System.out.println("Do you want the PP or the turdeau");
             System.out.println("Do you want the red puppet or the blue one");
         } else {
             System.out.println("If voting mattered would they let you do it?");
+        }
+        //Else If Statement
+        int year = 2024;
+        if(year == 2023) {
+            System.out.println("It is the current year -1 = " + year);
+        } else if (year == 2024) {
+            System.out.println("It is the current year : " + year);
+        } else {
+            System.out.println("All conditions failed");
         }
     }
 }

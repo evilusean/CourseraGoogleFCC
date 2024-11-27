be1b93dbca342fcce54bdc23117280e624852395
Sean Teams <shawntemesy@hotmail.com>
2024-11-27 08:14:48 -0400

    -

--- FCC-Java-Basics/FCC-Project/If-Statements/src/Main.java
+++ FCC-Java-Basics/FCC-Project/If-Statements/src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int age = 17;
+        int age = 18;
         //If Else statement
         if (age >= 18) {
             System.out.println("You can VOOOOOOOOOOTTTEEEEEEEE");
@@ -12,7 +12,7 @@ public class Main {
             System.out.println("If voting mattered would they let you do it?");
         }
         //Else If Statement
-        int year = 2024;
+        int year = 2023;
         if(year == 2023) {
             System.out.println("It is the current year -1 = " + year);
         } else if (year == 2024) {
@@ -26,6 +26,12 @@ public class Main {
             System.out.println("The user is active.");
         } else {
             System.out.println("The user is not active");
+        }
+        // '&&' and operator - both must be true to run
+        if (year == 2024 && age >= 18) {
+            System.out.println("It is the current year and voting doesn't matter");
+        } else {
+            System.out.println("so called representative democracy is a sham");
         }
     }
 }

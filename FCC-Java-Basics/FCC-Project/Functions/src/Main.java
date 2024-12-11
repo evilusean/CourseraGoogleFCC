08890ae5641b186ec766e295c5ceeffd1effd38a
Sean Teams <shawntemesy@hotmail.com>
2024-12-11 10:26:12 -0400

    -

--- FCC-Java-Basics/FCC-Project/Functions/src/Main.java
+++ FCC-Java-Basics/FCC-Project/Functions/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        sayHello("Sean");
+        sayHello("Sean", 33);
+        sayHello("Shawn", 35);
     }
-    public static void sayHello(String name) {
-        System.out.println("Hello " + name + "!");
+    public static void sayHello(String name, int age) {
+        System.out.println("Hello " + name + "! " + "You are " + age +" years old.");
     }
 }
 

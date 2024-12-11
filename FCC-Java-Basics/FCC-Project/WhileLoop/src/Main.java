0d223730a03b3dfb66aaeec78f917aee1703deaa
Sean Teams <shawntemesy@hotmail.com>
2024-12-11 08:21:56 -0400

    while loop
    do while loop

--- FCC-Java-Basics/FCC-Project/WhileLoop/src/Main.java
+++ FCC-Java-Basics/FCC-Project/WhileLoop/src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    int number = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (number<= 10) { //'while loop' will execute code until condition is true
+            number++;
+            System.out.println(number);
         }
+        do { //'do while loop' will do once, even if the while condition is false
+            number++;
+            System.out.println(number);
+        } while (number <= 1);
     }
 }

ecf540300ddc2aded092681493b516a66f63e71a
Sean Teams <shawntemesy@hotmail.com>
2024-12-11 06:42:57 -0400

    Update Main.java

--- FCC-Java-Basics/FCC-Project/ForLoops/src/Main.java
+++ FCC-Java-Basics/FCC-Project/ForLoops/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        for (int i = 0; i <= 200; i++) { //sets initial integer, conditions, and iterates +1
+            if (i == 100) {
+                break; //will break out of loop, stop the loop
+            }
+            if (i >= 30 && i <= 50) {
+                continue; //will skip while conditions are met
+            }
+            System.out.println("i = " + i);
+        }
     }
 }

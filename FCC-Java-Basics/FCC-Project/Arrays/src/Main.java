ab2b0413a8fc75eab2cd98491acd7e2f33f4e3a9
Sean Teams <shawntemesy@hotmail.com>
2024-12-12 01:02:58 -0400

    Arrays
    LeSeanDa Paul -ossa do no wen

--- FCC-Java-Basics/FCC-Project/Arrays/src/Main.java
+++ FCC-Java-Basics/FCC-Project/Arrays/src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //declare an array with type and '[]' name and '{}' values
+        int[] numbers = {12, 15, 60, 100, 200, 30}; //array of int
+        String[] friends = {"Sean", "Shawn", "Shaun", "LeSeanDa"}; //array of strings
+        for (int number: numbers) { // for each loop. each number in numbers, print
+            System.out.println(number);
+        }
+        for (int i = 0; i < numbers.length; i++) {//for each loop, '.length' methid
+            System.out.println(numbers[i]);
+        }
+        System.out.println(numbers[0]); //0 based indexing
+        for (String friend : friends) { //for each loop
+            System.out.println(friend);
         }
     }
 }

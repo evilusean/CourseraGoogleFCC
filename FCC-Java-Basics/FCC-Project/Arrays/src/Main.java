7d75b3f610f58cd15790031df798563de0027394
Sean Teams <shawntemesy@hotmail.com>
2024-12-12 02:26:54 -0400

    -

--- FCC-Java-Basics/FCC-Project/Arrays/src/Main.java
+++ FCC-Java-Basics/FCC-Project/Arrays/src/Main.java
@@ -3,6 +3,10 @@ public class Main {
         //declare an array with type and '[]' name and '{}' values
         int[] numbers = {12, 15, 60, 100, 200, 30}; //array of int
         String[] friends = {"Sean", "Shawn", "Shaun", "LeSeanDa"}; //array of strings
+        int[] numbers2 = new int[3]; //allows you to define a new array with set number of elements
+        numbers2[0] = 3; //manually input numbers into the array
+        numbers2[1] = 6;
+        numbers2[2] = 9;
         for (int number: numbers) { // for each loop. each number in numbers, print
             System.out.println(number);
         }

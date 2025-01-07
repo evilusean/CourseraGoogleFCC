98e656e14c0568cf94dd3dbdb3bdcd66e79f878d
Sean Teams <shawntemesy@hotmail.com>
2025-01-07 06:47:22 -0400

    -
    added default values for constructors

--- FCC-Java-Basics/FCC-Project/OOP/src/Car.java
+++ FCC-Java-Basics/FCC-Project/OOP/src/Car.java
@@ -1,15 +1,30 @@
-public class Car {
+public class Car { //each car object has 4 values
     private String name;
     private String model;
     private String color;
     private int doors;
 
-    public Car(String name, String model, String color, int doors) {
+    public Car(String name, String model, String color, int doors) { //constructor
         this.name = name;
         this.model = model;
         this.color = color;
         this.doors = doors;
     }
+
+    public Car() {
+        this("Name", "Model", "Color", 0); //this will allow you to input default values, will still call on the constructor above
+    }
+
+    public Car(String name) {
+        this(name, "Model", "Color", 0); //will provide default values except for name
+    }
+    public Car(String name, String model) {
+        this(name, model, "Color", 0); //will provide default values except for name and model
+    }
+    public Car(String name, String model, String color) {
+        this(name, model, color, 0); //will provide default values except for name, model, color
+    }
+
     public void move () {
         System.out.println("The car " + this.name + " is moving.");
     }
--- FCC-Java-Basics/FCC-Project/OOP/src/Main.java
+++ FCC-Java-Basics/FCC-Project/OOP/src/Main.java
@@ -2,13 +2,17 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car("Tesla", "CyberCuck", "Brown", 4);
         Car car2 = new Car("Toyota", "Tacoma", "Red", 2);
+        Car defaultcar = new Car();
         System.out.println(car2.getName()); //getters
         System.out.println(car.getName());
         System.out.println(car.getModel());
         System.out.println(car.getColor());
         System.out.println(car.getDoors());
+        System.out.println(defaultcar.getName());
 
         car.setName("tesla"); //setters
-        System.out.println(car.getName()); // tesla with a small 't'
+        System.out.println(car.getName()); // tesla with a small 't' - PE(w) ratio of 193(Lol)
+        car.move();
+        car.stop();
     }
 }

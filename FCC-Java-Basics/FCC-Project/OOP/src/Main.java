5eaabcf0774ab064fb5cb9195eee24101d7ab12e
Sean Teams <shawntemesy@hotmail.com>
2025-01-07 00:17:12 -0400

    -
    forgot to finish this, Java first then C#

--- FCC-Java-Basics/FCC-Project/OOP/src/Main.java
+++ FCC-Java-Basics/FCC-Project/OOP/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("Tesla", "CyberTruck", "Black", 4);
+        Car car = new Car("Tesla", "CyberCuck", "Black", 4);
         Car car2 = new Car("Toyota", "Tacoma", "Red", 2);
     }
 }

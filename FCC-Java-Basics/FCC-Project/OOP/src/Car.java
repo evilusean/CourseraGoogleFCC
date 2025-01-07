688bdd9118b7e09ecdad229dcbd625b40f1abbf0
Sean Teams <shawntemesy@hotmail.com>
2025-01-07 01:51:34 -0400

    getter and setter
    created getters and setters

--- FCC-Java-Basics/FCC-Project/OOP/src/Car.java
+++ FCC-Java-Basics/FCC-Project/OOP/src/Car.java
@@ -16,4 +16,38 @@ public class Car {
     public void stop () {
         System.out.println("The car " + this.name + " has stopped.");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
 }
+
+
--- FCC-Java-Basics/FCC-Project/OOP/src/Main.java
+++ FCC-Java-Basics/FCC-Project/OOP/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("Tesla", "CyberCuck", "Black", 4);
+        Car car = new Car("Tesla", "CyberCuck", "Brown", 4);
         Car car2 = new Car("Toyota", "Tacoma", "Red", 2);
+        System.out.println(car2.getName());
+        System.out.println(car.getName());
+        System.out.println(car.getModel());
+        System.out.println(car.getColor());
+        System.out.println(car.getDoors());
     }
 }

06878470ebaea7cdb79c1dc594bde12f7dd810cf
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-11 06:10:31 -0400

    -
    adding namespace, class, and static void Main

--- FCC-C#-Course-MiniProjects/Projects/HelloWorld/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/HelloWorld/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}

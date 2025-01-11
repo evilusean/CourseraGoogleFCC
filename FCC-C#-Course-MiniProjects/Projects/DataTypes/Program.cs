1787b17a97f272d2f35c2e8c3548039571d0d028
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-11 07:33:37 -0400

    datatypes boilerplate

---
+++ FCC-C#-Course-MiniProjects/Projects/DataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadLine();
+        }
+    }
+}

fccdb75335f56455d98ee29ae40ce11221d4fc01
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-13 12:46:51 -0400

    dotnet new console -n Project2-OddEven-Checker
    boilerplate

--- FCC-C#-Course-MiniProjects/Projects/Project1-Store-User-Data/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project1-Store-User-Data/Program.cs
@@ -2,13 +2,6 @@
 {
     class Program
     {
-        /*
-        Definite a variable to hold your name
-        Define a variable to hold your phone number
-        define a variable to hold your age
-        print variables line-by-line to the screen
-        Extra: define variables using the var keyword 
-        */
         static void Main(string[] args)
         {
             var name = "Sean Doe";

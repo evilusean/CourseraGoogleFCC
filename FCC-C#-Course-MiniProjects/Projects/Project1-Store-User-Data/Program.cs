2b7bf776751ce2952fbabe067f7eade8487e8b33
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-13 12:37:15 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project1-Store-User-Data/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project1-Store-User-Data/Program.cs
@@ -11,7 +11,12 @@
         */
         static void Main(string[] args)
         {
-            
+            var name = "Sean Doe";
+            var phoneNumber = "123-456-7890";
+            var age = 25;
+            System.Console.WriteLine(name);
+            System.Console.WriteLine(phoneNumber);
+            System.Console.WriteLine(age);
             Console.ReadLine();
         }
     }

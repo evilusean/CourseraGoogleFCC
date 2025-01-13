89bd96d97a7d99ab5a99d58893976068c8fbd7ab
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-13 12:57:19 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project2-OddEven-Checker/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project2-OddEven-Checker/Program.cs
@@ -12,7 +12,13 @@
         */
         static void Main(string[] args)
         {
-            // Write your code here
+            var var1 = 11;
+            var var2 = 15;
+            var checkEm = var1 % 2;
+            Console.WriteLine(checkEm);
+            var1 = 12;
+            checkEm = var1 % 2; //need to recalculater after change
+            Console.WriteLine(checkEm);
         }
     }
 }

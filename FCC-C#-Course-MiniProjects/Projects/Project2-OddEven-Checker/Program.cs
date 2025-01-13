32669bc7692e0393aee36768ad0b96efd4049b87
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-13 12:49:12 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project2-OddEven-Checker/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project2-OddEven-Checker/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Exercise2
+{
+    class Program
+    {
+        /*
+        Create and initialize two ints
+        make a variable to work out the remainder
+        output remainder to screen
+        change the first int variable to another number
+        and reclaculate the remainder
+        output the new remainder to the screen
+        */
+        static void Main(string[] args)
+        {
+            // Write your code here
+        }
+    }
+}

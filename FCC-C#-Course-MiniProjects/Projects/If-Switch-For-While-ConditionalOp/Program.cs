5febc20c432aa655f58770dbbae235a9ab2addcf
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-14 11:51:49 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/If-Switch-For-While-ConditionalOp/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/If-Switch-For-While-ConditionalOp/Program.cs
@@ -8,7 +8,8 @@ namespace CourseraGoogleFCC
         //for each example, so I'll just add them all to the same project
         static void Main(string[] args)
         {
-            IfStatementExample();
+            //IfStatementExample();
+            SwitchStatementExample();
         }
 
         // Boilerplate for if statements
@@ -41,7 +42,35 @@ namespace CourseraGoogleFCC
         // Boilerplate for switch statements
         static void SwitchStatementExample()
         {
-            // Add your switch statement code here
+            Console.Write("Enter a day of the week(#):");
+            int day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day");
+                    break;
+            }
         }
 
         // Boilerplate for for loops

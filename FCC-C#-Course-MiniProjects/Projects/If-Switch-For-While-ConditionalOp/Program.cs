0a9887f47b22f6d277e814370d0eb94b3a09235d
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-14 07:51:38 -0400

    Update Program.cs
    fixed boilerplate formatting, looks way cleaner to seperate like this and just call from the main function

--- FCC-C#-Course-MiniProjects/Projects/If-Switch-For-While-ConditionalOp/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/If-Switch-For-While-ConditionalOp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Boilerplate for for loops
+namespace CourseraGoogleFCC
+{
+    class Program
+    {
+        //last examples were very disordered, seperating each one into it's own part
+        //then just calling from the main function, I don't want a new project 
+        //for each example, so I'll just add them all to the same project
+        static void Main(string[] args)
+        {
+            IfStatementExample();
+        }
+
+        // Boilerplate for if statements
+        static void IfStatementExample()
+        {
+            // Add your if statement code here
+        }
+
+        // Boilerplate for switch statements
+        static void SwitchStatementExample()
+        {
+            // Add your switch statement code here
+        }
+
+        // Boilerplate for for loops
+        static void ForLoopExample()
+        {
+            // Add your for loop code here
+        }
+
+        // Boilerplate for while loops
+        static void WhileLoopExample()
+        {
+            // Add your while loop code here
+        }
+
+        // Boilerplate for conditional operators
+        static void ConditionalOperatorsExample()
+        {
+            // Add your conditional operators code here
+        }
+    }
+}

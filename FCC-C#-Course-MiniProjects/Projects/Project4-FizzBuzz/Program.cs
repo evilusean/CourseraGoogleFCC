d053833f307280d8ae9bf54d4b1fba9a2b582ce0
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-16 11:48:26 -0400

    boilerplate
    optimized fizzbuzz

--- FCC-C#-Course-MiniProjects/Projects/Project4-FizzBuzz/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project4-FizzBuzz/Program.cs
@@ -1,34 +1,26 @@
-﻿namespace Exercise
+﻿class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        /*
-        create a for loop from 1 to X(15)
-        3 and 5 = FizzBuzz
-        3 = Fizz
-        5 = Buzz
-        else = Number
-        */
-        static void Main(string[] args)
+        for (int i = 1; i <= 15; i++)
         {
-            for (int i = 1; i <= 15; i++)
+            bool threeDiv = i % 3 == 0;
+            bool fiveDiv = i % 5 == 0;
+            if (threeDiv && fiveDiv)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    System.Console.WriteLine("FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    System.Console.WriteLine("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    System.Console.WriteLine("Buzz");
-                }
-                else
-                {
-                    System.Console.WriteLine(i);
-                }
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (threeDiv)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (fiveDiv)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
             }
         }
     }

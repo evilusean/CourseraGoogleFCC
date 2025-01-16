bca0803e6ba88bfaeeab052142f1fb1b89568004
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-16 12:28:16 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/String-Literal-Formatting-Interpolation-Concat-Empty-EqualsFunc-iterationLoop-IsNullOrEmptyFunc/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/String-Literal-Formatting-Interpolation-Concat-Empty-EqualsFunc-iterationLoop-IsNullOrEmptyFunc/Program.cs
@@ -9,7 +9,12 @@
         }
 
         static void VerbatimStringLiteral() {
-
+            // '/' is the escape character, use 2 backslashes for a single backslash
+            string path = "C:\\Program Files\\Microsoft";
+            Console.WriteLine(path);
+            // Verbatim string literal, use '@' to ignore escape characters
+            string path2 = @"C:\Program Files\Microhard";
+            Console.WriteLine(path2);
         }
     }
 }

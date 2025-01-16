2be2bf83d1214634b85ec9df258b5638a5b67dbb
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-16 10:58:12 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project4-FizzBuzz/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project4-FizzBuzz/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Exercise
+{
+    class Program
+    {
+        /*
+        create a for loop from 1 to X(15)
+        3 and 5 = FizzBuzz
+        3 = Fizz
+        5 = Buzz
+        else = Number
+        */
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

daf72f4b4a77a6a81dcb66ebd8f7075bad2f906b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-16 12:04:13 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/String-Literal-Formatting-Interpolation-Concat-Empty-EqualsFunc-iterationLoop-IsNullOrEmptyFunc/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/String-Literal-Formatting-Interpolation-Concat-Empty-EqualsFunc-iterationLoop-IsNullOrEmptyFunc/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Strings
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            VerbatimStringLiteral();
+        }
+
+        static void VerbatimStringLiteral() {
+
+        }
+    }
+}

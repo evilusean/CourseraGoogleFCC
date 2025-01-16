403137ef5977c92b4f88af3a62146de9a518a730
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-16 09:41:06 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project3-TimesTable/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project3-TimesTable/Program.cs
@@ -6,6 +6,13 @@
         Write a for loop to print x time table */
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} x {i} = {number * i}");
+            }
             Console.ReadLine();
         }
     }

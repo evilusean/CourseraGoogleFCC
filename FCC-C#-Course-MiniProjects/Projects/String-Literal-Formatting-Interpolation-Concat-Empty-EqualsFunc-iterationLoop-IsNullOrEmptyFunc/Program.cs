ca4b986f6e5e373e17ee3e5a866fc70febc2ccb7
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-17 09:07:02 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/String-Literal-Formatting-Interpolation-Concat-Empty-EqualsFunc-iterationLoop-IsNullOrEmptyFunc/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/String-Literal-Formatting-Interpolation-Concat-Empty-EqualsFunc-iterationLoop-IsNullOrEmptyFunc/Program.cs
@@ -8,10 +8,12 @@
             //VerbatimStringLiteral();
             //StringFormatting();
             //StringInterpolation();
-            StringConcatenation();
+            //StringConcatenation();
+            StringEmpty();
         }
 
-        static void VerbatimStringLiteral() {
+        static void VerbatimStringLiteral()
+        {
             // '/' is the escape character, use 2 backslashes for a single backslash
             string path = "C:\\Program Files\\Microsoft";
             Console.WriteLine(path);
@@ -19,7 +21,8 @@
             string path2 = @"C:\Program Files\Microhard";
             Console.WriteLine(path2);
         }
-        static void StringFormatting() {
+        static void StringFormatting()
+        {
             // String formatting
             string name = "Sean";
             int age = 30;
@@ -30,18 +33,37 @@
             Console.WriteLine("Hello, my name is {0} and I am {1} years old.", name, age);
         }
 
-        static void StringInterpolation() {
+        static void StringInterpolation()
+        {
             string name = "Sean";
             int age = 30;
             // String interpolation
             Console.WriteLine($"Hello, my name is {name} and I am {age} years old.");
         }
-        static void StringConcatenation() {
+        static void StringConcatenation()
+        {
             string firstName = "Sean";
             int age = 33;
             //yet another way to concatenate strings with ',' instead of '+'
             string test = string.Concat("Hello, my name is ", firstName, " and I am ", age, " years old.");
             Console.WriteLine(test);
         }
+        static void StringEmpty()
+        {
+            string name = ""; //initialize with an empty string
+            string name2 = string.Empty; //initialize with an empty string
+            Console.WriteLine(string.IsNullOrEmpty(name)); //true
+            Console.WriteLine(string.IsNullOrEmpty(name2)); //true
+
+            Console.Write("Enter your name: ");
+            string input = Console.ReadLine();
+
+            //if (input != "") { //if string is empty, return false'!=' not equal, run code
+            if (input != string.Empty) {// easier to read/understand same as above
+                Console.WriteLine($"Hello, {input}!");
+        } else {
+                Console.WriteLine("You did not enter a name.");
+            }
+}
     }
 }

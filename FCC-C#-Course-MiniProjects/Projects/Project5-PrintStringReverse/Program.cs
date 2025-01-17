1a131dd9f5c2a06b598f9e89f318a950a25e4cbf
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-17 11:23:29 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project5-PrintStringReverse/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project5-PrintStringReverse/Program.cs
@@ -6,10 +6,28 @@
         Ask user to input message
         print in order
         print in reverse
+        character by character
         */
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a message: ");
+            string message = Console.ReadLine();
 
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(50);
+            }
+
+            Console.Write("-");
+
+            for (int i = message.Length - 1; i >= 0; i--)
+            {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(50);
+            }
+
+            Console.ReadLine();
         }
     }
 }

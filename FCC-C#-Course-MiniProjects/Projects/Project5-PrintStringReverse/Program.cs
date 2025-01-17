73f1b20579e81129f0217063f4c40e3c77101b74
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-17 11:12:01 -0400

    boilerplate

---
+++ FCC-C#-Course-MiniProjects/Projects/Project5-PrintStringReverse/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise
+{
+    class Program
+    {
+        /* 
+        Ask user to input message
+        print in order
+        print in reverse
+        */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

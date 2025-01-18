38d8b399bac173e87044c7604a35e8b416a2bedc
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-18 12:01:25 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project6-PasswordChecker/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project6-PasswordChecker/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Project6
+{
+    class Program
+    {
+        /*
+        Ask user to enter a password, and store it
+        ask user to enter password again, and store
+        check they both contain something
+        check if they are both the same
+        if they are print 'passwords' match
+        if they are not print 'passwords do not match'
+        if the are empty, print "Please enter a password"
+        */
+        static void Main(string[] args)
+        {
+            PasswordChecker passwordChecker = new PasswordChecker();
+            passwordChecker.Run();
+        }
+    }
+}

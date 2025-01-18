8bf262a091adc8dd5b27282b88681bf2403a2aff
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-18 12:49:30 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Arrays-Sorting-Reversal-Clearing-IndexOf-Lists-Dicts/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Arrays-Sorting-Reversal-Clearing-IndexOf-Lists-Dicts/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arrays();
+        }
+        static void Arays () {
+            // Arrays
+            // Arrays are a collection of elements of the same type
+            // Arrays are fixed in size
+            // Arrays are zero-based
+            // Arrays are reference types
+        }
+    }
+}

d5381cbaa627c52ceb6baa1a12f67be46f1040a8
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-18 13:43:14 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Arrays-Sorting-Reversal-Clearing-IndexOf-Lists-Dicts/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Arrays-Sorting-Reversal-Clearing-IndexOf-Lists-Dicts/Program.cs
@@ -6,14 +6,24 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Arrays();
+            ArraysExample();
         }
-        static void Arays () {
+
+        static void ArraysExample()
+        {
             // Arrays
             // Arrays are a collection of elements of the same type
             // Arrays are fixed in size
             // Arrays are zero-based
             // Arrays are reference types
+
+            // Below syntax: type[] name = new type[size];
+            int[] numbers = new int[3];
+
+            numbers[0] = 5;
+            numbers[1] = 10;
+            numbers[2] = 15;
+            Console.WriteLine($"{numbers[0]}, {numbers[1]}, {numbers[2]}");
         }
     }
 }

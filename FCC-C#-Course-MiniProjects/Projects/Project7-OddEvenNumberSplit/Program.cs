1c52f4a98db59facbe4f5a1274ee113cff3fcbd9
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-23 12:53:21 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project7-OddEvenNumberSplit/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project7-OddEvenNumberSplit/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    /*
+    Create two lists with integer data type, one for even number and one for odd number.
+    loop from 0-20
+    if the number is even, add it to the even list
+    if the number is odd, add it to the odd list
+    print the even list
+    print the odd list
+    */
+    static void Main(string[] args) {
+        Console.ReadLine();
+    }
+}

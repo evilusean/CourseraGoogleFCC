faf693dc7d4362bbd05678d5d07f6d3afc75782a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-24 08:29:32 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project8-ArrayOfMultiples/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project8-ArrayOfMultiples/Program.cs
@@ -3,16 +3,26 @@
     class Program
     {
         /* 
-        Defin and intialize two integers (num, length)
+        Define and intialize two integers (num, length)
         (7, 5) -> [7, 14, 21, 28, 35]
         (17, 6) -> [17, 34, 51, 68, 85, 102]
         (3, 7) -> [3, 6, 9, 12, 15, 18, 21]
         loop through and insert the (loop counter x num) into the array
         print the final array
         */
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            
+            int num = 7;
+            int length = 5;
+            int[] result = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = num * (i + 1);
+            }
+            foreach (var item in result)
+            {
+                System.Console.Write(item + " ");
+            }
             Console.ReadLine();
         }
     }

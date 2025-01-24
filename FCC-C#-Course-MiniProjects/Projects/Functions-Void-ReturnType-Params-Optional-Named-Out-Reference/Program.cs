b5d3fb972e459e38af77216b2603d329de18ace5
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-24 11:25:31 -0400

    boilerplate

--- FCC-C#-Course-MiniProjects/Projects/Functions-Void-ReturnType-Params-Optional-Named-Out-Reference/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Functions-Void-ReturnType-Params-Optional-Named-Out-Reference/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionExample();
+        }
+
+        static void FunctionExample()
+        {
+            // Call the function with named parameters
+            Console.ReadLine();
+        }
+    }
+}

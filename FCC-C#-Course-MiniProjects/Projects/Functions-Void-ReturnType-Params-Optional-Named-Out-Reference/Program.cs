1cc92a30adaf92dc033e43bdde4bccccc22f5e74
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-24 12:57:14 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Functions-Void-ReturnType-Params-Optional-Named-Out-Reference/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Functions-Void-ReturnType-Params-Optional-Named-Out-Reference/Program.cs
@@ -4,13 +4,26 @@
     {
         static void Main(string[] args)
         {
-            FunctionExample();
+            //FunctionExample();
+            VoidExample();
         }
 
         static void FunctionExample()
         {
             // Call the function with named parameters
+            Console.WriteLine("This is a FuncSean example");
             Console.ReadLine();
+        }
+
+        static void VoidExample()
+        {
+            // void functions do not return a value
+            Console.WriteLine("This is a Void example");
+            int[] numbers = { 1, 2, 3, 4, 5 }; //even though this is an array, it just prints it, not returning it
+            foreach (int number in numbers)//this is still a void, it just prints the numbers
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }

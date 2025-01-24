4ae8558926a9ebb254d30871230b2388178d09e9
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-24 08:08:44 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project8-ArrayOfMultiples/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project8-ArrayOfMultiples/Program.cs
@@ -2,9 +2,18 @@
 {
     class Program
     {
+        /* 
+        Defin and intialize two integers (num, length)
+        (7, 5) -> [7, 14, 21, 28, 35]
+        (17, 6) -> [17, 34, 51, 68, 85, 102]
+        (3, 7) -> [3, 6, 9, 12, 15, 18, 21]
+        loop through and insert the (loop counter x num) into the array
+        print the final array
+        */
         public static void Main(string[] args)
         {
-
+            
+            Console.ReadLine();
         }
     }
 }

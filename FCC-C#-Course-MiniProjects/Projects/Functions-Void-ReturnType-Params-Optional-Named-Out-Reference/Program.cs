42fa2dcfbfc0f2f907e58cc650a48b7cd6b62025
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-25 01:52:35 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Functions-Void-ReturnType-Params-Optional-Named-Out-Reference/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Functions-Void-ReturnType-Params-Optional-Named-Out-Reference/Program.cs
@@ -1,13 +1,16 @@
-﻿namespace Functions
+﻿using System;
+
+namespace Functions
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             //FunctionExample();
-            VoidExample();
+            //VoidExample();
+            ReturnExample();
         }
-
         static void FunctionExample()
         {
             // Call the function with named parameters
@@ -21,6 +24,59 @@
             Console.WriteLine("This is a Void example");
             int[] numbers = { 1, 2, 3, 4, 5 }; //even though this is an array, it just prints it, not returning it
             foreach (int number in numbers)//this is still a void, it just prints the numbers
+            {
+                Console.WriteLine(number);
+            }
+        }
+        static string returnName() // this is a return function, it returns a string
+        {
+            return "Sean";
+        }
+
+        static int returnAge() // this is a return function, it returns an int
+        {
+            return 25;
+        }
+
+        static int ReadNumberFromConsole()
+        {
+            Console.WriteLine("Enter a number: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static int[] CreateArray() // creates an array without asking for user input that gets returned
+        {
+            int[] numbers = new int[] // initialize the array with values
+            {
+                0, 1, 2
+            };
+            return numbers; // return the array of 3 numbers
+        }
+
+        // combining the two return functions into a single function
+        static void ReturnExample()
+        {
+            string name = returnName();
+            int age = returnAge();
+            Console.WriteLine("Name: " + name + ". Age: " + age);
+
+            // use return functions to return arrays
+            int[] numbers = new int[3]; // will create an array of 3 numbers using the for loop that gets them from a return
+
+            for (int i = 0; i < numbers.Length; i++) // for loop to get user inputs
+            {
+                numbers[i] = ReadNumberFromConsole(); // found below, will ask user for inputs and give them to this func
+            }
+
+            //will display all the numbers user input from the return function
+            foreach (int number in numbers) // will print the numbers
+            {
+                Console.WriteLine(number);
+            }
+
+            // Will display the numbers from the return function without user input
+            int[] newNumbers = CreateArray(); // will get inputs without user input
+            foreach (int number in newNumbers) // will print the numbers
             {
                 Console.WriteLine(number);
             }

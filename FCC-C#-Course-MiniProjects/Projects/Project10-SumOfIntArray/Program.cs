9e1304ff1b4810b2a31ab3ebdbc035af2f7a112d
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-28 10:04:13 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project10-SumOfIntArray/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project10-SumOfIntArray/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program 
+{
+    /*
+    Create and initialize int array of numbers
+    create function SumOfNumbers with int return type
+    int array param
+    function should return total of all numbers
+    call in main and output the total
+    extra: check array length
+        return -1 if array empty
+        check return in main and output message
+        do we need to return -1, how else can we make this?
+    */
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+    }
+}

44940a207da3f04d0009c3c5b1d5e17152dc3f8a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-28 07:33:07 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project9-AreaOfTriangle/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project9-AreaOfTriangle/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Triangle
+{
+    class Program {
+        /*
+        ask user for width and height, store them
+        create a function to calculate the area
+        function should calculate the area using: (width * height) / 2
+        call in main and print out the area of the triangle
+        */
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}

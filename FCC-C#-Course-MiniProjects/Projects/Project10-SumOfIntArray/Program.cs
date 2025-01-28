dd229cf021aecd1758f33caecbb437182f9e09b4
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-28 10:38:30 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Project10-SumOfIntArray/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Project10-SumOfIntArray/Program.cs
@@ -1,18 +1,63 @@
-﻿class Program 
+﻿using System;
+
+namespace Project10_SumOfIntArray
 {
-    /*
-    Create and initialize int array of numbers
-    create function SumOfNumbers with int return type
-    int array param
-    function should return total of all numbers
-    call in main and output the total
-    extra: check array length
-        return -1 if array empty
-        check return in main and output message
-        do we need to return -1, how else can we make this?
-    */
-    static void Main(string[] args)
+    class Program
     {
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            // check return in main and output message
+            if (SumOfNumbers(numbers, out int sum))
+            {
+                Console.WriteLine($"The Total is : {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Array is Empty");
+            }
+            /*
+            int result = SumOfNumbers(numbers);
+            if (result > -1)
+            {
+                Console.WriteLine($"The Total is: {result});
+            }
+            else
+            {
+                Console.WriteLine("Can not add an empty array");
+            }
+            */
+        }
+
+        // return a bool and int, new method
+        static bool SumOfNumbers(int[] numbers, out int sum)
+        {
+            sum = 0; // Initialize sum
+            if (numbers.Length > 0)
+            {
+                foreach (var item in numbers)
+                {
+                    sum += item;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        /* easy method, just check length(if 0 return -1) and return sum
+        static int SumOfNumbers(int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return -1;
+            }
+            int sum = 0;
+            foreach (var item in numbers)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+        */
     }
 }

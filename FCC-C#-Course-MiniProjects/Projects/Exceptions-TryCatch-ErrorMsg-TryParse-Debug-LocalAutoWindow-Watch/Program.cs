3f63803bf9420d13f6633a353fc01b77bcea7a37
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-30 09:34:48 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Exceptions-TryCatch-ErrorMsg-TryParse-Debug-LocalAutoWindow-Watch/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Exceptions-TryCatch-ErrorMsg-TryParse-Debug-LocalAutoWindow-Watch/Program.cs
@@ -6,7 +6,8 @@
         static void Main(string[] args)
         {
             //ExceptionsExample();
-            TryCatchExample();
+            //TryCatchExample();
+            PrintErrorExample();
         }
         static void ExceptionsExample()
         {
@@ -22,6 +23,8 @@
                     Console.Write("Enter a number :"); 
                     int num = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine(num);
+
+                    looping = false;//add this so that it doesn't create an infinite loop
                 }
                 catch (OverflowException) 
                 {
@@ -35,6 +38,24 @@
                 {
                     Console.WriteLine("Something has went wrong!"); 
                     // no matter what exception, will always print, all exceptions, with no exception
+                }
+            }
+        }
+        static void PrintErrorExample() //how to output different messages to catch block
+        {
+            bool looping = true; // best practice for scope is to wrap it in a while loop, until user entered a correct num
+            while (looping) {//while looping is true, keep asking for a number
+                try 
+                {
+                    Console.Write("Enter a number :"); 
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(num);
+
+                    looping = false;//add this so that it doesn't create an infinite loop
+                }
+                catch (Exception e) //added a new parameter 'e', which allows us to do more with the exception
+                {
+                    Console.WriteLine($"Error: {e.Message}"); //will print error message depending on the exception
                 }
             }
         }

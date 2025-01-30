983933dd32d8356822988bf4191f10f0df8518e0
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-01-30 08:16:18 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Exceptions-TryCatch-ErrorMsg-TryParse-Debug-LocalAutoWindow-Watch/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Exceptions-TryCatch-ErrorMsg-TryParse-Debug-LocalAutoWindow-Watch/Program.cs
@@ -5,16 +5,25 @@
 
         static void Main(string[] args)
         {
-            ExceptionsExample();
+            //ExceptionsExample();
+            TryCatchExample();
         }
         static void ExceptionsExample()
         {
-            Console.Write("Enter a number :");
+            Console.Write("Enter a number :"); //enter in a wrong format or too large number, not much here
             int num = Convert.ToInt32(Console.ReadLine());
         }
         static void TryCatchExample()
         {
-            
+            try 
+            {
+                Console.Write("Enter a number :"); 
+                int num = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something has went wrong!"); // no matter what exception, will always print, all exceptions
+            }
         }
     }
 }

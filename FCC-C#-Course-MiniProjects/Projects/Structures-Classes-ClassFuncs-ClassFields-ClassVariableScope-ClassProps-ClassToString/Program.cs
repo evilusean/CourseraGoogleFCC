bf5b1fb8bc507e2b2f9a986cb3d0dd1014b0e90c
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-01 11:33:50 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Structures-Classes-ClassFuncs-ClassFields-ClassVariableScope-ClassProps-ClassToString/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Structures-Classes-ClassFuncs-ClassFields-ClassVariableScope-ClassProps-ClassToString/Program.cs
@@ -18,6 +18,15 @@ namespace StructuresExample
         {
             public string name; // new keyword = public = access modifier, so we can use below
             public int age;
+            public int birthmonth;
+
+            //Constructor - all parameters are local(only to constructor, use 'this.' to go to current scope of struct)
+            public Person(string name, int age, int birthMonth)
+            {
+                this.name = name;
+                this.age = age;
+                this.birthmonth = birthmonth;
+            }
         }
 
         static void StructuresExample()
@@ -25,7 +34,32 @@ namespace StructuresExample
             Person person; // will create the person structure variable
             person.name = "Sean"; // how to add data to the 'person.name' structure variable above
             person.age = 33;
-            Console.WriteLine($"{person.name} - {person.age}");
+            person.birthmonth = 4;;
+            Console.WriteLine($"{person.name} - {person.age} - {person.birthmonth}");
+
+            Person person2 = ReturnPerson(); //use below function to 
+            Console.WriteLine($"{person2.name} - {person2.age} - {person2.birthmonth}");
+
+        }
+        static Person ReturnPerson() // return a person structure
+        {
+            Console.Write("Enter your name: ");//get user inputs
+            string name2 = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your birthmonth #: ");
+            int birthmonth2 = Convert.ToInt32(Console.ReadLine());
+
+            Person person2; //create the new structure
+            person2.name = name2; //update with user inputs into the newly created structure
+            person2.age = age2;
+            person2.birthmonth = birthmonth2;
+            //will return the manual inputs person - commented out, can only return one
+            //return person2; 
+
+            return new Person(name2, age2, birthmonth2); //will use the constructor to return values 'new' keyword
         }
     }
 }

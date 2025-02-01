8da8e9042a068e9d331c86affc12a50bfddd600a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-01 10:51:31 -0400

    Update Program.cs

--- FCC-C#-Course-MiniProjects/Projects/Structures-Classes-ClassFuncs-ClassFields-ClassVariableScope-ClassProps-ClassToString/Program.cs
+++ FCC-C#-Course-MiniProjects/Projects/Structures-Classes-ClassFuncs-ClassFields-ClassVariableScope-ClassProps-ClassToString/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Ling;
+//using System.Ling;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Classes {
-    class Program 
+namespace StructuresExample
+{
+    class Program
     {
-        static void main(strings[] args)
+        static void Main(string[] args)
         {
             StructuresExample();
         }
+
+        // Structures allow you to store different datatypes inside one datatype, a group of datatypes
+        struct Person
+        {
+            public string name; // new keyword = public = access modifier, so we can use below
+            public int age;
+        }
+
         static void StructuresExample()
         {
-
+            Person person; // will create the person structure variable
+            person.name = "Sean"; // how to add data to the 'person.name' structure variable above
+            person.age = 33;
+            Console.WriteLine($"{person.name} - {person.age}");
         }
     }
 }

191ee76dae1c361c8c228270a0cd6a2c8b96aa0f
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-14 11:12:49 -0400

    Update game.js
    you let me into my zone -ye

--- FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
+++ FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
@@ -44,6 +44,25 @@ preload() {
             gameObject.x = dragX;
             gameObject.y = dragY;
         })
+        this.input.on('dragstart', function (pointer, gameObject) {
+            gameObject.setTint(0xff69b4);
+            self.children.bringToTop(gameObject);
+        })
+
+        this.input.on('dragend', function (pointer, gameObject, dropped) {
+            gameObject.setTint();
+            if (!dropped) {
+                gameObject.x = gameObject.input.dragStartX;
+                gameObject.y = gameObject.input.dragStartY;
+            }
+        })
+
+        this.input.on('drop', function (pointer, gameObject, dropZone) {
+            dropZone.data.values.cards++;
+            gameObject.x = (dropZone.x - 350) + (dropZone.data.values.cards * 50);
+            gameObject.y = dropZone.y;
+            gameObject.disableInteractive();
+        })
     }
 
     update() {

c5298f8fa9b795624c4cf5c1984b137a7ec976d2
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-14 12:01:14 -0400

    test server works

--- FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
+++ FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
@@ -1,5 +1,6 @@
 import Card from '../helpers/card';
 import Zone from '../helpers/zone';
+import io from 'socket.io-client';
 export default class Game extends Phaser.Scene {
     constructor() {
         super({
@@ -15,6 +16,11 @@ preload() {
 }
 
     create() {
+        this.socket = io('http://localhost:3000');
+
+        this.socket.on('connect', function () {
+            console.log('Connected!');
+        });
         this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
         let self = this;
         this.zone = new Zone(this);

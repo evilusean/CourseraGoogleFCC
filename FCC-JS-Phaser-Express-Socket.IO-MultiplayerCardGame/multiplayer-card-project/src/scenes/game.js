665ce2e0b35baa867ef59fe221c808643f97cbad
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-14 07:13:53 -0400

    Update game.js

---
+++ FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
@@ -0,0 +1,47 @@
+export default class Game extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'Game'
+        });
+    }
+
+    preload() {
+        this.load.image('cyanCardFront', 'src/assets/CyanCardFront.png');
+        this.load.image('cyanCardBack', 'src/assets/CyanCardBack.png');
+        this.load.image('magentaCardFront', 'src/assets/MagentaCardFront.png');
+        this.load.image('magentaCardBack', 'src/assets/MagentaCardBack.png');
+    }
+
+    create() {
+        this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
+        let self = this;
+
+        this.card = this.add.image(300, 300, 'cyanCardFront').setScale(0.3, 0.3).setInteractive();
+        this.input.setDraggable(this.card);
+
+        this.dealCards = () => {
+
+        }
+
+        this.dealText.on('pointerdown', function () {
+            self.dealCards();
+        })
+
+        this.dealText.on('pointerover', function () {
+            self.dealText.setColor('#ff69b4');
+        })
+
+        this.dealText.on('pointerout', function () {
+            self.dealText.setColor('#00ffff');
+        })
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+        })
+    }
+
+    update() {
+
+    }
+}

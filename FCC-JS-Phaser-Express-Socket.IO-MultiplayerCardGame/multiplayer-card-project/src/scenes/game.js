576188b1f37119664667e9e5cd7d3f76b4f1a1ee
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-14 10:15:46 -0400

    Update game.js
    fixed cards,  apparently that 'src' was messing it up

--- FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
+++ FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
@@ -5,12 +5,12 @@ export default class Game extends Phaser.Scene {
         });
     }
 
-    preload() {
-        this.load.image('cyanCardFront', 'src/assets/CyanCardFront.png');
-        this.load.image('cyanCardBack', 'src/assets/CyanCardBack.png');
-        this.load.image('magentaCardFront', 'src/assets/MagentaCardFront.png');
-        this.load.image('magentaCardBack', 'src/assets/MagentaCardBack.png');
-    }
+preload() {
+    this.load.image('cyanCardFront', 'assets/CyanCardFront.png');
+    this.load.image('cyanCardBack', 'assets/CyanCardBack.png');
+    this.load.image('magentaCardFront', 'assets/MagentaCardFront.png');
+    this.load.image('magentaCardBack', 'assets/MagentaCardBack.png');
+}
 
     create() {
         this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();

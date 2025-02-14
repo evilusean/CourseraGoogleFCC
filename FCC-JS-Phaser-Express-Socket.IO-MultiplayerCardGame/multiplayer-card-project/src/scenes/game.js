8b4ca2cfca88bc4f03b8a5d21d0c3350b326b745
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-14 12:09:32 -0400

    Update game.js

--- FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
+++ FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
@@ -16,10 +16,36 @@ preload() {
 }
 
     create() {
-        this.socket = io('http://localhost:3000');
-
-        this.socket.on('connect', function () {
-            console.log('Connected!');
+        const server = require('express')();
+        const http = require('http').createServer(server);
+        const io = require('socket.io')(http);
+        let players = [];
+        
+        io.on('connection', function (socket) {
+            console.log('A user connected: ' + socket.id);
+        
+            players.push(socket.id);
+        
+            if (players.length === 1) {
+                io.emit('isPlayerA');
+            };
+        
+            socket.on('dealCards', function () {
+                io.emit('dealCards');
+            });
+        
+            socket.on('cardPlayed', function (gameObject, isPlayerA) {
+                io.emit('cardPlayed', gameObject, isPlayerA);
+            });
+        
+            socket.on('disconnect', function () {
+                console.log('A user disconnected: ' + socket.id);
+                players = players.filter(player => player !== socket.id);
+            });
+        });
+        
+        http.listen(3000, function () {
+            console.log('Server started!');
         });
         this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
         let self = this;

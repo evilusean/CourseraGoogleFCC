bdc27c05b878d9c29ed2c2aaa33851be0a0a4562
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-15 01:37:28 -0400

    Update game.js

--- FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
+++ FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
@@ -16,6 +16,9 @@ preload() {
 }
 
     create() {
+        this.isPlayerA = false;
+        this.opponentCards = [];
+        
         const server = require('express')();
         const http = require('http').createServer(server);
         const io = require('socket.io')(http);

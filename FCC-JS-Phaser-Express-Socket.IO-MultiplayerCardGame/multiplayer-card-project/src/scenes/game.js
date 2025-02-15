0d69c19aa4ddef0677856ee59d735b1a17cc93d9
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-15 08:49:32 -0400

    Update game.js

--- FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
+++ FCC-JS-Phaser-Express-Socket.IO-MultiplayerCardGame/multiplayer-card-project/src/scenes/game.js
@@ -1,12 +1,15 @@
-import Card from '../helpers/card';
-import Zone from '../helpers/zone';
 import io from 'socket.io-client';
+import Card from '../helpers/card';
+import Dealer from "../helpers/dealer";
+import Zone from '../helpers/zone';
+
 export default class Game extends Phaser.Scene {
     constructor() {
         super({
             key: 'Game'
         });
     }
+
 
 preload() {
     this.load.image('cyanCardFront', 'assets/CyanCardFront.png');
@@ -15,108 +18,85 @@ preload() {
     this.load.image('magentaCardBack', 'assets/MagentaCardBack.png');
 }
 
-    create() {
-        this.isPlayerA = false;
-        this.opponentCards = [];
-       
-        this.socket.on('dealCards', function () {
-            self.dealer.dealCards();
-            self.dealText.disableInteractive();
-        })
+create() {
+    this.isPlayerA = false;
+    this.opponentCards = [];
 
-        this.socket.on('cardPlayed', function (gameObject, isPlayerA) {
-            if (isPlayerA !== self.isPlayerA) {
-                let sprite = gameObject.textureKey;
-                self.opponentCards.shift().destroy();
-                self.dropZone.data.values.cards++;
-                let card = new Card(self);
-                card.render(((self.dropZone.x - 350) + (self.dropZone.data.values.cards * 50)), (self.dropZone.y), sprite).disableInteractive();
-            }
-        })
+    this.zone = new Zone(this);
+    this.dropZone = this.zone.renderZone();
+    this.outline = this.zone.renderOutline(this.dropZone);
 
-        const server = require('express')();
-        const http = require('http').createServer(server);
-        const io = require('socket.io')(http);
-        let players = [];
-        
-        io.on('connection', function (socket) {
-            console.log('A user connected: ' + socket.id);
-        
-            players.push(socket.id);
-        
-            if (players.length === 1) {
-                io.emit('isPlayerA');
-            };
-        
-            socket.on('dealCards', function () {
-                io.emit('dealCards');
-            });
-        
-            socket.on('cardPlayed', function (gameObject, isPlayerA) {
-                io.emit('cardPlayed', gameObject, isPlayerA);
-            });
-        
-            socket.on('disconnect', function () {
-                console.log('A user disconnected: ' + socket.id);
-                players = players.filter(player => player !== socket.id);
-            });
-        });
-        
-        http.listen(3000, function () {
-            console.log('Server started!');
-        });
-        this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
-        let self = this;
-        this.zone = new Zone(this);
-        this.dropZone = this.zone.renderZone();
-        this.outline = this.zone.renderOutline(this.dropZone);
+    this.dealer = new Dealer(this);
 
-        this.dealCards = () => {
-            for (let i = 0; i < 5; i++) {
-                let playerCard = new Card(this);
-                playerCard.render(475 + (i * 100), 650, 'cyanCardFront');
-            }
+    let self = this;
+
+    this.socket = io('http://localhost:3000');
+
+    this.socket.on('connect', function () {
+        console.log('Connected!');
+    });
+
+    this.socket.on('isPlayerA', function () {
+        self.isPlayerA = true;
+    })
+
+    this.socket.on('dealCards', function () {
+        self.dealer.dealCards();
+        self.dealText.disableInteractive();
+    })
+
+    this.socket.on('cardPlayed', function (gameObject, isPlayerA) {
+        if (isPlayerA !== self.isPlayerA) {
+            let sprite = gameObject.textureKey;
+            self.opponentCards.shift().destroy();
+            self.dropZone.data.values.cards++;
+            let card = new Card(self);
+            card.render(((self.dropZone.x - 350) + (self.dropZone.data.values.cards * 50)), (self.dropZone.y), sprite).disableInteractive();
         }
+    })
 
-        this.dealText.on('pointerdown', function () {
-            self.socket.emit("dealCards");
-        })
+    this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
 
-        this.dealText.on('pointerover', function () {
-            self.dealText.setColor('#ff69b4');
-        })
+    this.dealText.on('pointerdown', function () {
+        self.socket.emit("dealCards");
+    })
 
-        this.dealText.on('pointerout', function () {
-            self.dealText.setColor('#00ffff');
-        })
+    this.dealText.on('pointerover', function () {
+        self.dealText.setColor('#ff69b4');
+    })
 
-        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-            gameObject.x = dragX;
-            gameObject.y = dragY;
-        })
-        this.input.on('dragstart', function (pointer, gameObject) {
-            gameObject.setTint(0xff69b4);
-            self.children.bringToTop(gameObject);
-        })
+    this.dealText.on('pointerout', function () {
+        self.dealText.setColor('#00ffff');
+    })
 
-        this.input.on('dragend', function (pointer, gameObject, dropped) {
-            gameObject.setTint();
-            if (!dropped) {
-                gameObject.x = gameObject.input.dragStartX;
-                gameObject.y = gameObject.input.dragStartY;
-            }
-        })
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+    })
 
-        this.input.on('drop', function (pointer, gameObject, dropZone) {
-            dropZone.data.values.cards++;
-            gameObject.x = (dropZone.x - 350) + (dropZone.data.values.cards * 50);
-            gameObject.y = dropZone.y;
-            gameObject.disableInteractive();
-            self.socket.emit('cardPlayed', gameObject, self.isPlayerA);
-        })
-    }
+    this.input.on('dragstart', function (pointer, gameObject) {
+        gameObject.setTint(0xff69b4);
+        self.children.bringToTop(gameObject);
+    })
 
-    update() {
+    this.input.on('dragend', function (pointer, gameObject, dropped) {
+        gameObject.setTint();
+        if (!dropped) {
+            gameObject.x = gameObject.input.dragStartX;
+            gameObject.y = gameObject.input.dragStartY;
+        }
+    })
 
-    }
+    this.input.on('drop', function (pointer, gameObject, dropZone) {
+        dropZone.data.values.cards++;
+        gameObject.x = (dropZone.x - 350) + (dropZone.data.values.cards * 50);
+        gameObject.y = dropZone.y;
+        gameObject.disableInteractive();
+        self.socket.emit('cardPlayed', gameObject, self.isPlayerA);
+    })
+}
+
+update() {
+
+}
 }

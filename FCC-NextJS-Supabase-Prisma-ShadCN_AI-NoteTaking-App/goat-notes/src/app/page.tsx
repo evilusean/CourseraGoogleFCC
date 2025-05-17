366f40b0e854b7646e5d53f5e1afff8e01537268
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-05-17 20:53:38 -0300

    Update page.tsx

--- FCC-NextJS-Supabase-Prisma-ShadCN_AI-NoteTaking-App/goat-notes/src/app/page.tsx
+++ FCC-NextJS-Supabase-Prisma-ShadCN_AI-NoteTaking-App/goat-notes/src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function page() {
+function HomePage() {
   return (
-    <div>page</div>
+    <div>HomePage</div>
   )
 }
 
-export default page
+export default HomePage

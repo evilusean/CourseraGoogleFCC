3bab7d0a039ef2f7078f5fade066cd7f218692d9
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-05-24 14:42:06 -0300

    Create server.ts

---
+++ FCC-NextJS-Supabase-Prisma-ShadCN_AI-NoteTaking-App/goat-notes/src/auth/server.ts
@@ -0,0 +1,40 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export async function createClient() {
+  const cookieStore = await cookies();
+
+  const client = createServerClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            );
+          } catch {}
+        },
+      },
+    },
+  );
+
+  return client;
+}
+
+export async function getUser() {
+  const { auth } = await createClient();
+
+  const userObject = await auth.getUser();
+
+  if (userObject.error) {
+    console.error(userObject.error);
+    return null;
+  }
+
+  return userObject.data.user;
+}

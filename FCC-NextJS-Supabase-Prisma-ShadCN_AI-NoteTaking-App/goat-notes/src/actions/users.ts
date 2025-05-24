153d625e69c07de572198b5a900022141b2e0810
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-05-24 14:36:01 -0300

    Create users.ts

---
+++ FCC-NextJS-Supabase-Prisma-ShadCN_AI-NoteTaking-App/goat-notes/src/actions/users.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { createClient } from "@/auth/server";
+import { prisma } from "@/db/prisma";
+import { handleError } from "@/lib/utils";
+
+export const loginAction = async (email: string, password: string) => {
+  try {
+    const { auth } = await createClient();
+
+    const { error } = await auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) throw error;
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const logOutAction = async () => {
+  try {
+    const { auth } = await createClient();
+
+    const { error } = await auth.signOut();
+    if (error) throw error;
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const signUpAction = async (email: string, password: string) => {
+  try {
+    const { auth } = await createClient();
+
+    const { data, error } = await auth.signUp({
+      email,
+      password,
+    });
+    if (error) throw error;
+
+    const userId = data.user?.id;
+    if (!userId) throw new Error("Error signing up");
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        email,
+      },
+    });
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};

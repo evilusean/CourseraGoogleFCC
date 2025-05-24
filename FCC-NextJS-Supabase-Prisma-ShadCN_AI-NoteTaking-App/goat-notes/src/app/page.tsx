a234f9533a271fd4180b94c33bfc0e4f8fd1bce3
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-05-24 14:41:06 -0300

    Update page.tsx

--- FCC-NextJS-Supabase-Prisma-ShadCN_AI-NoteTaking-App/goat-notes/src/app/page.tsx
+++ FCC-NextJS-Supabase-Prisma-ShadCN_AI-NoteTaking-App/goat-notes/src/app/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { getUser } from "@/auth/server";
+import AskAIButton from "@/components/AskAIButton";
+import NewNoteButton from "@/components/NewNoteButton";
+import NoteTextInput from "@/components/NoteTextInput";
+import HomeToast from "@/components/HomeToast";
+import { prisma } from "@/db/prisma";
 
-function HomePage() {
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+async function HomePage({ searchParams }: Props) {
+  const noteIdParam = (await searchParams).noteId;
+  const user = await getUser();
+
+  const noteId = Array.isArray(noteIdParam)
+    ? noteIdParam![0]
+    : noteIdParam || "";
+
+  const note = await prisma.note.findUnique({
+    where: { id: noteId, authorId: user?.id },
+  });
+
   return (
-    <div>HomePage</div>
-  )
+    <div className="flex h-full flex-col items-center gap-4">
+      <div className="flex w-full max-w-4xl justify-end gap-2">
+        <AskAIButton user={user} />
+        <NewNoteButton user={user} />
+      </div>
+
+      <NoteTextInput noteId={noteId} startingNoteText={note?.text || ""} />
+
+      <HomeToast />
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;

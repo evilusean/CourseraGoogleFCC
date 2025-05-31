4d200b475c16c9deaa435d591097015bbb7da467
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-05-31 11:07:10 -0300

    Update myApp.js

--- FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Express/myApp.js
+++ FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Express/myApp.js
@@ -1,9 +1,10 @@
 let express = require("express");
+let path = require("path");
 let app = express();
 
 console.log("Hello World");
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 module.exports = app;

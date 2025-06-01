e0453add1835068cf86caaa32278b2db87731064
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-06-01 13:42:32 -0300

    Update myApp.js

--- FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Express/myApp.js
+++ FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Express/myApp.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 let express = require("express");
 let path = require("path");
 let app = express();
@@ -10,7 +11,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  res.json({ message });
 });
 
 module.exports = app;

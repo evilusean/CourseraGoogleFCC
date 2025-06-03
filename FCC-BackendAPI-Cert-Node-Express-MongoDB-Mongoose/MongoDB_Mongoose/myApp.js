150fd3dfcc20829591d61c8cd409c3b53bda6337
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-06-03 12:18:47 -0300

    Update myApp.js

--- FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/MongoDB_Mongoose/myApp.js
+++ FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/MongoDB_Mongoose/myApp.js
@@ -1,4 +1,10 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 

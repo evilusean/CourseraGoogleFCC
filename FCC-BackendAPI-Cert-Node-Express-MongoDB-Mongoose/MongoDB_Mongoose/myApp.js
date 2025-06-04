848d73eb67159cab4b2ff15ad65c171f7fd9009e
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-06-04 11:12:15 -0300

    Update myApp.js

--- FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/MongoDB_Mongoose/myApp.js
+++ FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/MongoDB_Mongoose/myApp.js
@@ -6,7 +6,13 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);

caf77d802ca23c356de1ae42668c8ed9f2abc636
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-06-05 13:31:07 -0300

    Update myApp.js

--- FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/MongoDB_Mongoose/myApp.js
+++ FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/MongoDB_Mongoose/myApp.js
@@ -101,7 +101,14 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort("name")
+    .limit(2)
+    .select("-age")
+    .exec((err, data) => {
+      if (err) return done(err);
+      return done(null, data);
+    });
 };
 
 /** **Well Done !!**

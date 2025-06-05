bac8f0dfcb84e28ceb2b36d01f89dab7701bdce1
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-06-05 11:01:11 -0300

    Update myApp.js

--- FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/MongoDB_Mongoose/myApp.js
+++ FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/MongoDB_Mongoose/myApp.js
@@ -58,7 +58,14 @@ const findPersonById = (personId, done) => {
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    if (err) return done(err);
+    person.favoriteFoods.push(foodToAdd);
+    person.save((err, updatedPerson) => {
+      if (err) return done(err);
+      return done(null, updatedPerson);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {

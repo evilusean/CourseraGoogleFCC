a95e4d4d86b4d50d11afd14be382ff8f876d7be6
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-06-11 19:52:02 -0300

    npm install

--- FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Project1-TimeStampMicroService/index.js
+++ FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Project1-TimeStampMicroService/index.js
@@ -2,31 +2,54 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+// Timestamp Microservice endpoint
+app.get("/api/timestamp/:date_string?", (req, res) => {
+  let dateString = req.params.date_string;
+  let date;
 
+  if (!dateString) {
+    date = new Date();
+  } else {
+    // if dateString contains only digits, treat it as a Unix timestamp in milliseconds
+    if (/^\d+$/.test(dateString)) {
+      date = new Date(parseInt(dateString));
+    } else {
+      date = new Date(dateString);
+    }
+  }
+
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });

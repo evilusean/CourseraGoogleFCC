87df14ae3957cc4967559c8249dbfbcd178aaa1a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-06-13 12:42:22 -0300

    Update index.js

--- FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Project2-RequestHeaderParser/index.js
+++ FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Project2-RequestHeaderParser/index.js
@@ -1,13 +1,17 @@
-// index.js
-// where your node app starts
-
-// init project
+// Init express app, get env variables if present
 require("dotenv").config();
 var express = require("express");
 var app = express();
 
-// set to trust proxy headers
-app.set("trust proxy", true);
+// Log incoming requests in development:
+if (process.env.RUN_MODE === "development") {
+  app.use((req, res, next) => {
+    console.log(
+      `${req.method} ${req.path}; IP=${req.ip}; https?=${req.secure}`
+    );
+    next();
+  });
+}
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -17,37 +21,36 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
+// Return index.html on GET request to '/'
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
-});
-
-// new endpoint to parse header information
-app.get("/api/whoami", function (req, res) {
-  const ipaddress = (
-    req.headers["x-forwarded-for"] ||
-    req.ip ||
-    req.connection.remoteAddress ||
-    ""
-  )
-    .split(",")[0]
-    .trim();
-  const language = req.headers["accept-language"];
-  const software = req.headers["user-agent"];
-
-  res.json({
-    ipaddress: ipaddress,
-    language: language,
-    software: software,
+// Return request header info on API call
+app.get("/api/whoami", (req, res) => {
+  req.headers;
+  return res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
   });
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+// 404 page not found:
+app.get("*", (req, res) => {
+  // Redirect to index
+  res.redirect("/");
+});
+
+// Internal Error Handler:
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server error: See Server Logs");
+});
+
+// Listen for requests on given PORT, else 3000
+// http://localhost:3000/
+var listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });

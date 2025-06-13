f9d6c8003f8513f82ae6f76e34a2ecb779152ff6
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-06-13 13:05:12 -0300

    Update index.js

--- FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Project2-RequestHeaderParser/index.js
+++ FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Project2-RequestHeaderParser/index.js
@@ -1,17 +1,12 @@
-// Init express app, get env variables if present
+// index.js
+// where your node app starts
+
+// init project
 require("dotenv").config();
 var express = require("express");
 var app = express();
 
-// Log incoming requests in development:
-if (process.env.RUN_MODE === "development") {
-  app.use((req, res, next) => {
-    console.log(
-      `${req.method} ${req.path}; IP=${req.ip}; https?=${req.secure}`
-    );
-    next();
-  });
-}
+//---------------- boilerplate ----------------
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -21,44 +16,50 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// Return index.html on GET request to '/'
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Enable trust proxy - important for getting correct IP when behind a proxy
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+// Enable trust proxy to get correct IP behind reverse proxies
 app.enable("trust proxy");
 
-// Return request header info on API call
+// Header parser endpoint
 app.get("/api/whoami", function (req, res) {
-  // Simplify IP address handling
-  let ipaddress = req.ip;
+  // Get IP address with fallbacks
+  let ipaddress =
+    req.headers["x-forwarded-for"] ||
+    req.ip ||
+    req.connection.remoteAddress ||
+    "127.0.0.1";
+
+  // Clean up IP address
+  if (ipaddress.includes(",")) {
+    ipaddress = ipaddress.split(",")[0];
+  }
   if (ipaddress.includes("::ffff:")) {
     ipaddress = ipaddress.split("::ffff:")[1];
   }
+  if (ipaddress === "::1") {
+    ipaddress = "127.0.0.1";
+  }
 
-  res.json({
-    ipaddress: ipaddress,
+  // Create response object
+  const responseObj = {
+    ipaddress: ipaddress.trim(),
     language: req.headers["accept-language"],
     software: req.headers["user-agent"],
-  });
-});
+  };
 
-// 404 page not found:
-app.get("*", (req, res) => {
-  // Redirect to index
-  res.redirect("/");
-});
-
-// Internal Error Handler:
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Internal Server error: See Server Logs");
-});
-
-// Listen for requests on given PORT, else 3000
-// http://localhost:3000/
-var listener = app.listen(process.env.PORT || 3000, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+  res.json(responseObj);
 });

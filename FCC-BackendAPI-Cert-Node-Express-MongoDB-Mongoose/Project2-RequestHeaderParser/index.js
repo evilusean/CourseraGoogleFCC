562837fc4f1ea0e7f28c7c31d672292f0e7b1345
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-06-13 18:25:18 -0300

    Update index.js
    going back to the simplest implementation, there was a massive outage yesterday, tester might be broken, will come back to it in a few days, after I finish other parts of course

--- FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Project2-RequestHeaderParser/index.js
+++ FCC-BackendAPI-Cert-Node-Express-MongoDB-Mongoose/Project2-RequestHeaderParser/index.js
@@ -7,37 +7,32 @@ var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
 var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// Configure express for proxy support
-app.set("trust proxy", true);
-
-// Static files middleware
+// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// Basic routing
+// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// First API endpoint
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-// Request Header Parser endpoint
-app.get("/api/whoami", function (req, res) {
-  var input = req.headers;
-
-  res.json({
-    ipaddress: req.ip,
-    language: input["accept-language"],
-    software: input["user-agent"],
-  });
-});
-
-// Start server
+// listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
+});
+
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.acceptsLanguages(),
+    software: req.header("user-agent"),
+  });
 });

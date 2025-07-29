f23d0165a4301f9c7feed1854009d655cca9aeab
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-07-29 16:03:26 -0300

    Update 2_functional-tests.js

--- FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-mochachai/tests/2_functional-tests.js
+++ FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-mochachai/tests/2_functional-tests.js
@@ -67,11 +67,16 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+Browser.site = 'http://0.0.0.0:3000'; // Your URL here
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
+  const browser = new Browser();
 
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {

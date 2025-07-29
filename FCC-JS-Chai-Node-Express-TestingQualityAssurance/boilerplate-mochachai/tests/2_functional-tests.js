98f6759c8a3dbb1b41578a56431015ef04c59c86
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-07-29 15:39:18 -0300

    Update 2_functional-tests.js

--- FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-mochachai/tests/2_functional-tests.js
+++ FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-mochachai/tests/2_functional-tests.js
@@ -28,8 +28,8 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });

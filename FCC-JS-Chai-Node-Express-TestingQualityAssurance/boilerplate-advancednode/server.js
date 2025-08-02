2de9f6ec6ecb9cc0f98b43643cd7ff5a375379ef
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-08-02 14:50:20 -0300

    Update server.js

--- FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-advancednode/server.js
+++ FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-advancednode/server.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+// I just spent 2 days trying to get this to work, it runs locally, but the tester on FCC is not passing it no matter what I do
+// I'm taking a break, just got another level in WankiKani for Japanese and I need to update my vocab with the new words.
+// Hopefully when I come back, something works, Future Sean Problems :
+// https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/set-up-a-template-engine
+// https://forum.freecodecamp.org/t/advanced-node-and-express/567135
+// above is the 'help' forum for this challenge, where I literally just copied and pasted what they give me, it should work, but it doesn't.
 
 const app = express();
 

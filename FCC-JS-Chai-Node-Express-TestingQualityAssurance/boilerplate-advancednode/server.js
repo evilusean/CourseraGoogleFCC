bb5168f6de82a3087fa89b72ae45faebb5ecc953
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-08-02 14:13:57 -0300

    Update server.js

--- FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-advancednode/server.js
+++ FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-advancednode/server.js
@@ -6,21 +6,19 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-// Set Pug as the view engine
 app.set('view engine', 'pug');
+app.set('views', './views/pug');
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Set views directory to ./views/pug
-app.set('views', __dirname + '/views/pug');
-
-// Add fccTesting for testing
-fccTesting(app);
-
-// Home page route
-app.get('/', (req, res) => {
+app.route('/').get((req, res) => {
+  // Change the response to render the Pug template
   res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log(`Listening on port ${PORT}`);
 });

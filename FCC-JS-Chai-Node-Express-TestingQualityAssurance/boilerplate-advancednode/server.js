9b72b4aa3de697c3894bcedf48395e67928d44e7
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-08-04 20:16:25 -0300

    Update server.js

--- FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-advancednode/server.js
+++ FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-advancednode/server.js
@@ -3,20 +3,23 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-// I just spent 2 days trying to get this to work, it runs locally, but the tester on FCC is not passing it no matter what I do
-// I'm taking a break, just got another level in WankiKani for Japanese and I need to update my vocab with the new words.
-// Hopefully when I come back, something works, Future Sean Problems :
-// https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/set-up-a-template-engine
-// https://forum.freecodecamp.org/t/advanced-node-and-express/567135
-// above is the 'help' forum for this challenge, where I literally just copied and pasted what they give me, it should work, but it doesn't.
-
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
-
-const cors = require('cors');
-app.use(cors());
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());

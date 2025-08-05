8496d9880dd932970542a43d807a4c3f69140d7c
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-08-05 15:19:47 -0300

    Update server.js

--- FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-advancednode/server.js
+++ FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-advancednode/server.js
@@ -25,35 +25,37 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
 fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
-});
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please log in'
+    });
+  });
 
-passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  // });
-});
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
 
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
+});
+  
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);

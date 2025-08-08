613576a0b8e92f66eb6fef7ac7e2077440622702
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-08-08 14:19:22 -0300

    Update server.js

--- FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-advancednode/server.js
+++ FCC-JS-Chai-Node-Express-TestingQualityAssurance/boilerplate-advancednode/server.js
@@ -36,6 +36,11 @@ myDB(async client => {
 
   routes(app, myDataBase);
   auth(app, myDataBase);
+
+  io.on('connection', socket => {
+    console.log('A user has connected');
+  });
+  
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
@@ -43,6 +48,6 @@ myDB(async client => {
 });
   
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });

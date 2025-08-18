a63aad18ec5441fae82b994a7cb981adbf1b6b48
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-08-18 14:41:28 -0300

    Update 2_functional-tests.js

--- FCC-JS-Chai-Node-Express-TestingQualityAssurance/Project4-SudokuSolver/tests/2_functional-tests.js
+++ FCC-JS-Chai-Node-Express-TestingQualityAssurance/Project4-SudokuSolver/tests/2_functional-tests.js
@@ -6,6 +6,28 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
+  test('Solve a puzzle with valid puzzle string: POST request to /api/solve', function(done) {
+    chai.request(server)
+      .post('/api/solve')
+      .send({ puzzle: '1'.repeat(81) })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'solution');
+        done();
+      });
+  });
 
+  test('Solve a puzzle with missing puzzle string: POST request to /api/solve', function(done) {
+    chai.request(server)
+      .post('/api/solve')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, { error: 'Required field missing' });
+        done();
+      });
+  });
+
+  // Add more tests for each FCC requirement...
 });
 

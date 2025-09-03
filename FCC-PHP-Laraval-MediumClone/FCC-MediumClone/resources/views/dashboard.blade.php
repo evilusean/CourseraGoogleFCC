f2154232ba0487742a176db7320b9a4df1261b2a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-03 11:40:46 -0300

    Update dashboard.blade.php

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/dashboard.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/dashboard.blade.php
@@ -1,15 +1,38 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+
+
+                    <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active"
+                                aria-current="page">Tab 1</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#"
+                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
+                                2</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#"
+                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
+                                3</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#"
+                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
+                                4</a>
+                        </li>
+                        <li>
+                            <a class="inline-block px-4 py-3 text-gray-400 cursor-not-allowed dark:text-gray-500">Tab
+                                5</a>
+                        </li>
+                    </ul>
+
                 </div>
             </div>
         </div>

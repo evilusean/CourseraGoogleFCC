c36fa56b18bf3a701ac4ddf2cc4023c8da82df1a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-03 17:03:17 -0300

    Update dashboard.blade.php

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/dashboard.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/dashboard.blade.php
@@ -1,31 +1,30 @@
 <x-app-layout>
 
 
-    <div class="py-12">
+    <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-
-
+                <div class="p-4 text-gray-900">
                     <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
                         <li class="me-2">
-                            <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active"
-                                aria-current="page">Tab 1</a>
-                        </li>
-                        <li class="me-2">
-                            <a href="#"
-                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
-                                2</a>
+                            <a href="#" class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active"
+                                aria-current="page">All</a>
                         </li>
                         @foreach ($categories as $category)
                         <li class="me-2">
                             <a href="#"
-                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">
+                                class="inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">
                                 {{ $category->name }}
                             </a>
                         </li>
                         @endforeach
                     </ul>
+
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-8">
+                <div class="p-4 text-gray-900">
+                    
 
                 </div>
             </div>

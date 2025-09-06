aeb4bf82c18e0e4f166719d9497c7025ab48662b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-06 18:54:51 -0300

    Update navigation.blade.php

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/layouts/navigation.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/layouts/navigation.blade.php
@@ -10,12 +10,7 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
+                
             </div>
 
             <!-- Settings Dropdown -->

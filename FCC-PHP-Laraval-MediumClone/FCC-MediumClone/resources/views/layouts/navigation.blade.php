a4340e85a6a5e551cab0924f88846c3745169239
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-07 16:25:29 -0300

    Update navigation.blade.php

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/layouts/navigation.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/layouts/navigation.blade.php
@@ -13,6 +13,13 @@
                 
             </div>
 
+            <div class="flex">
+                        <a href="/test" class="flex items-center">
+            <x-primary-button>
+                Create Post
+            </x-primary-button>
+            </a>
+
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
@@ -55,6 +62,7 @@
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
+            </div>
             </div>
         </div>
     </div>

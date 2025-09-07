5a56a1e90503cadcc36ab1addcd8ba6114be2cef
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-07 18:21:11 -0300

    Update create.blade.php

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/post/create.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/post/create.blade.php
@@ -3,8 +3,17 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
                 <form action="{{ route('post.store') }}" method="post">
-                    <!-- Title -->
+                    <!-- Image -->
                     <div>
+                        <x-input-label for="image" :value="__('Image')" />
+                        <x-text-input id="image" class="block mt-1 w-full" type="file" name="image"
+                            :value="old('image')" autofocus />
+                        <x-input-error :messages="$errors->get('image')" class="mt-2" />
+                    </div>
+
+
+                    <!-- Title -->
+                    <div class="mt-4">
                         <x-input-label for="title" :value="__('Title')" />
                         <x-text-input id="title" class="block mt-1 w-full" type="text" name="title"
                             :value="old('title')" required autofocus />
@@ -13,7 +22,7 @@
                     <!-- Content -->
                     <div class="mt-4">
                         <x-input-label for="content" :value="__('Content')" />
-                        <x-input-textarea id="content" class="block mt-1 w-full" type="text" name="content"
+                        <x-input-textarea id="content" class="block mt-1 w-full" name="content"
                             :value="old('content')" required ></x-input-textarea>
                         <x-input-error :messages="$errors->get('content')" class="mt-2" />
                     </div>

d6b06e23160e7b05ec4d9e878942e73ccad1fd93
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-11 21:17:55 -0300

    right side complete

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/components/user-avatar.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/components/user-avatar.blade.php
@@ -1,9 +1,9 @@
-@props(['user'])
+@props(['user', 'size' => 'w-12 h-12'])
 
 @if ($user->image)
-    <img class="h-12 w-12 rounded-full" src="{{ $user->imageUrl() }}" alt="{{ $user->name }}">
+    <img class="{{ $size }} rounded-full" src="{{ $user->imageUrl() }}" alt="{{ $user->name }}">
 @else
-    <img class="h-12 w-12 rounded-full"
+    <img class="{{ $size }} rounded-full"
         src="https://raw.githubusercontent.com/evilusean/DaGram-InstagramClone/refs/heads/main/Images/NP%20Seans/NPC%20SoiFace.png"
         alt="NPC">
 @endif
--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/profile/show.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/profile/show.blade.php
@@ -4,11 +4,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                <div class="flex">
-                    <div>
+                    <div class="flex-1">
                     
                     </div>
-                    <div class="w-[320px] border-l">
-                        <x-user-avatar :user="$user" />
+                    <div class="w-[320px] border-l px-8">
+                        <x-user-avatar :user="$user" size="w-24 h-24" />
+                        <h3>{{ $user->name }}</h3>
+                        <p class="text-gray-500">26k followers</p>
+                        <p>
+                            {{ $user->bio }}
+                        </p>
+                        <div class="mt-4">
+                            <button class="bg-emerald-600 rounded-full px-4 py-2 text-white">
+                                Follow
+                            </button>
+                        </div>
                     </div>
                </div>
             </div>

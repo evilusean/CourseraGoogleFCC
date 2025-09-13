04a76da443bee5623baa767c3719bb19882a8980
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-12 22:07:20 -0300

    Update User.php

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/app/Models/User.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/app/Models/User.php
@@ -100,14 +100,16 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->hasMany(Post::class);
     }
 
+
+
     public function following()
     {
-        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
     }
 
     public function followers()
     {
-        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
     }
 
     public function imageUrl()

a55f898987957ed7fe223f5887db5a6ae63502f4
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-12 21:51:47 -0300

    Update 2025_09_13_004635_create_followers_table.php

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/database/migrations/2025_09_13_004635_create_followers_table.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/database/migrations/2025_09_13_004635_create_followers_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('followers', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('follower_id')->constrained('users')->onDelete('cascade');
+            
+            $table->timestamp('created_at')->nullable();
         });
     }
 

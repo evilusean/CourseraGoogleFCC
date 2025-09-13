f27644fac585f11682f1ab57b0f8d65eaac00684
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-12 22:37:58 -0300

    following check
    dynamic following

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/app/Models/User.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/app/Models/User.php
@@ -119,4 +119,9 @@ class User extends Authenticatable implements MustVerifyEmail
         }
         return null;
     }
+
+    public function isFollowedBy(User $user)
+    {
+        return $this->followers()->where('follower_id', $user->id)->exists();
+    }
 }
--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/profile/show.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/profile/show.blade.php
@@ -14,7 +14,9 @@
                             @endforelse
                         </div>
                     </div>
-                    <div class="w-[320px] border-l px-8">
+                    <div x-data="{
+                        following: {{ $user->isFollowedBy(auth()->user()) ? 'true' : 'false' }},
+                    }" class="w-[320px] border-l px-8">
                         <x-user-avatar :user="$user" size="w-24 h-24" />
                         <h3>{{ $user->name }}</h3>
                         <p class="text-gray-500">
@@ -23,13 +25,15 @@
                         <p>
                             {{ $user->bio }}
                         </p>
-                        @auth
+                        @if (auth()->user() && auth()->user()->id !== $user->id)
                             <div class="mt-4">
-                                <button class="bg-emerald-600 rounded-full px-4 py-2 text-white">
-                                    Follow
+                                <button class="bg-emerald-600 rounded-full px-4 py-2 text-white"
+                                x-text="following ? 'Unfollow' : 'Follow'"
+                                >
+                                    
                                 </button>
                             </div>
-                        @endauth
+                        @endif
                     </div>
                 </div>
             </div>

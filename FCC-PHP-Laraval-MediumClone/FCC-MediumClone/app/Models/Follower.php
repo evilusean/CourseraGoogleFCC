1be7df2d32a83ff041b643e34ae9e0a68332ddc4
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-12 21:57:38 -0300

    Update Follower.php

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/app/Models/Follower.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/app/Models/Follower.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Follower extends Model
 {
-    //
+    protected const UPDATED_AT = null;
+    protected $fillable = ['user_id', 'follower_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function followers()
+    {
+        return $this->belongsTo(User::class, 'follower_id');
+    }
 }
+

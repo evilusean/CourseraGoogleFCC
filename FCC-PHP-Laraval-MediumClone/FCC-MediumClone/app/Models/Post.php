5989defb69a340bece806c2cc3e263fd69a340d3
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-13 20:50:23 -0300

    -

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/app/Models/Post.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/app/Models/Post.php
@@ -46,6 +46,11 @@ class Post extends Model
         return $this->belongsTo(Category::class);
     }
 
+    public function claps()
+    {
+        return $this->hasMany(Clap::class);
+    }
+
     public function readTime($wordsPerMinute = 100)
     {
         $wordCount = str_word_count(strip_tags($this->content));

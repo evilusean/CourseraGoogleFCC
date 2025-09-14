fd9faa10f288e09051c1003f2b6889e0bb371a53
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-13 21:20:56 -0300

    Update clap-button.blade.php

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/components/clap-button.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/components/clap-button.blade.php
@@ -1,6 +1,16 @@
 @props(['count' => 0])
 
-<div class="mt-8 p-4 border-t border-b">
+<div x-data="{
+    count: {{ $count }},
+    clap() {
+        axios.post().then(response => {
+            this.count = response.data.count;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }
+}" class="mt-8 p-4 border-t border-b">
     <button class="text-gray-500 hover:text-gray-700 flex gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="size-6">

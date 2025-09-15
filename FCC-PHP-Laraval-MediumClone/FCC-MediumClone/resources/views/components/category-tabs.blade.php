dba5382b062bd97450fb2756cd22f379abf18733
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-14 21:38:12 -0300

    Update category-tabs.blade.php
    fuxed 'all' highlighting

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/components/category-tabs.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/components/category-tabs.blade.php
@@ -1,6 +1,11 @@
 <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
     <li class="me-2">
-        <a href="#" class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active" aria-current="page">All</a>
+        <a href="/" class="{{
+         request('category') 
+         ? 'inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white' 
+         : 'inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active' }}">
+            All
+        </a>
     </li>
     @foreach ($categories as $category)
         <li class="me-2">

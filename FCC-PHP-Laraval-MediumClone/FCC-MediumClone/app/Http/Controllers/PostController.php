9602d9bbbb34e183eb2e7bbd7b3e3a8d5b639ca5
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-14 21:06:07 -0300

    Update PostController.php

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/app/Http/Controllers/PostController.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/app/Http/Controllers/PostController.php
@@ -126,4 +126,11 @@ class PostController extends Controller
     {
         //
     }
+    public function category(Category $category)
+    {
+        $posts = $category->posts()->simplePaginate(5);
+        return view('post.index', [
+            'posts' => $posts,
+        ]);
+    }
 }

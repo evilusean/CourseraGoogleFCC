59e02c5231489b32c1b81d5175e107707157ceff
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-09-14 21:36:00 -0300

    Update category-tabs.blade.php
    changed tab highlighting on selected tab

--- FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/components/category-tabs.blade.php
+++ FCC-PHP-Laraval-MediumClone/FCC-MediumClone/resources/views/components/category-tabs.blade.php
@@ -5,7 +5,11 @@
     @foreach ($categories as $category)
         <li class="me-2">
             <a href="{{ route('post.byCategory', $category) }}"
-                class="inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">
+                class="{{ 
+                    Route::currentRouteNamed('post.byCategory') && request('category')->id == $category->id 
+                    ? 'inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active' 
+                    : 'inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white' 
+                }}">
                 {{ $category->name }}
             </a>
         </li>
